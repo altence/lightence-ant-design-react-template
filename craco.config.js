0c0bb3d4f400e369076da864136501f786aec824
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-06-15 17:19:19 +0300

    feat(craco): update craco config to be able ot analyze build

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,16 @@
 const CracoLessPlugin = require('craco-less');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const WebpackBar = require('webpackbar');
+
+process.env.BROWSER = 'none';
 
 module.exports = {
+  webpack: {
+    plugins: [
+      new WebpackBar({ profile: true }),
+      ...(process.env.NODE_ENV === 'development' ? [new BundleAnalyzerPlugin({ openAnalyzer: false })] : []),
+    ],
+  },
   plugins: [
     {
       plugin: CracoLessPlugin,
--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,7 +1,8 @@
 import { ReportHandler } from 'web-vitals';
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
+  if (onPerfEntry) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
