0927da9c5341a4f1a15c33f3d6ed3551d62dfe84
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-06-15 10:56:01 +0300

    feat(app): basic antd setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './styles/main.less'
 
 ReactDOM.render(
   <React.StrictMode>
