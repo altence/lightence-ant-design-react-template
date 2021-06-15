d792d5787c1df995ce2702e294004a0aeb9f79ab
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-06-15 17:41:31 +0300

    refactoring(app): remove test components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Button } from 'antd';
-import Test from './components/Test';
 
 const App: React.FC = () => {
   return (
     <div>
       <header>
+        HELLO WORLD
         <Button type="primary">TEST</Button>
       </header>
-      <Test testVal={4} />
     </div>
   );
 };
