68f2637a5ae10a04ad9e0e2e294bedb5bb2416aa
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-06-16 16:25:42 +0300

    feat(styles): install and setup `styledComponents`

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { routes } from './routes/routes';
+import GlobalStyle from './styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
 const App: React.FC = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <Switch>
-          {routes.map((route, index) => (
-            <Route {...route} key={index} />
-          ))}
-        </Switch>
-      </BrowserRouter>
-    </div>
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Switch>
+            {routes.map((route, index) => (
+              <Route {...route} key={index} />
+            ))}
+          </Switch>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 };
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+export const colors = {
+  main: 'black',
+  secondary: 'white',
+};
+
+const theme: DefaultTheme = {
+  colors,
+};
+
+export default theme;
