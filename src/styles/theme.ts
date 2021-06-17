624f9d5e7988c01583e7f769abe7153ab06c0351
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-06-17 20:24:11 +0300

    feat(sc): add media breakpoints

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,12 +1,24 @@
-import { DefaultTheme } from 'styled-components';
-
 export const colors = {
   primary: '#088a98',
   secondary: 'white',
 };
 
-const theme: DefaultTheme = {
+export const breakpoints = {
+  md: 768,
+  xl: 1200,
+};
+
+const getMedia = (breakpoint: number) => `(min-width: ${breakpoint}px)`;
+
+export const media = {
+  md: getMedia(breakpoints.md),
+  xl: getMedia(breakpoints.xl),
+};
+
+const theme = {
   colors,
+  breakpoints,
+  media,
 };
 
 export default theme;
