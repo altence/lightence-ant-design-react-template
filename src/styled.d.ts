0959651917a63f2cd4af8ceeb08d2a2bc3c472d8
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-06-17 19:34:08 +0300

    feat(sc): update `styled components` typings

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,10 +1,7 @@
 import 'styled-components';
+import theme from './styles/theme';
 
 declare module 'styled-components' {
-  export interface DefaultTheme {
-    colors: {
-      main: string;
-      secondary: string;
-    };
-  }
+  type Theme = typeof theme;
+  export type DefaultTheme = Theme;
 }
