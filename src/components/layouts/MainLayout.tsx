8fcbdf1235d0fa373faeffac40d971922dbb1c06
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-06-18 16:51:38 +0300

    feat(layout): add condition to define mobile or desktop layout

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import MobileLayout from './mobile/MobileLayout';
 import SiderContent from '../sider/SiderContent';
+import DesktopLayout from './desktop/DesktopLayout';
+import { useMediaQuery } from 'react-responsive';
+import theme from '../../styles/theme';
 
 const MainLayout: React.FC = ({ children }) => {
-  return <MobileLayout siderContent={<SiderContent />}>{children}</MobileLayout>;
+  const isDesktop = useMediaQuery({
+    query: theme.media.md,
+  });
+
+  const Layout = isDesktop ? DesktopLayout : MobileLayout;
+
+  return <Layout siderContent={<SiderContent />}>{children}</Layout>;
 };
 
 export default MainLayout;
