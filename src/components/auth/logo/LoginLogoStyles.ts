29fbd644317e9ff1eb95c16210eecc0daf68ba61
AlexKarmaz <karmazct@mail.ru>
2021-07-02 12:54:56 +0300

    feat(login): add login layout and logo

---
+++ src/components/auth/logo/LoginLogoStyles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const LogoDiv = styled.div`
+  padding-top: 6.5rem;
+`;
+
+export const LogoText = styled.a`
+  font-weight: 700;
+  font-size: 4rem;
+  color: #ffffff;
+`;
