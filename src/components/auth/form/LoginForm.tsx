55541fa28dc8c06542bbeb39b1123bf318c2ffe2
AlexKarmaz <karmazct@mail.ru>
2021-07-14 21:09:05 +0300

    feat(login): add login form

--- src/components/auth/form/LoginForm.tsx
+++ src/components/auth/form/LoginForm.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { FormDiv, FormStyled, FormTitle } from './LoginFormStyles';
 
 const LoginForm: React.FC = () => {
-  return <div>Form</div>;
+  const handleSubmit = () => {
+    console.log('Received values of form');
+  };
+
+  return (
+    <FormDiv>
+      <FormTitle>Login</FormTitle>
+      <FormStyled name="normal_login" className="login-form" initialValues={{ remember: true }} onFinish={handleSubmit}>
+        <Form.Item name="username" rules={[{ required: true, message: 'Please input your Username!' }]}>
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item name="password" rules={[{ required: true, message: 'Please input your Password!' }]}>
+          <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <a className="login-form-forgot" href="">
+            Forgot password
+          </a>
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </Form.Item>
+      </FormStyled>
+    </FormDiv>
+  );
 };
 
 export default LoginForm;
---
+++ src/components/auth/form/LoginFormStyles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { Form } from 'antd';
+
+export const FormDiv = styled.div`
+  width: 22rem;
+  heigh: 26.75rem;
+  background: white;
+  padding: 2rem;
+`;
+
+export const FormStyled = styled(Form)``;
+
+export const FormTitle = styled.h2`
+  font-weight: 700;
+`;
