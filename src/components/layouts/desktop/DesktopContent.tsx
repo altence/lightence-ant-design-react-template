ab73a972b0fa80599a1441d036e8f672881abf92
armistrice <armistrice@gmail.com>
2021-07-27 19:07:34 +0300

    feat(activitychart): made and added activity chart
    fix(layouts): added semicolon

--- src/components/layouts/desktop/DesktopContent.tsx
+++ src/components/layouts/desktop/DesktopContent.tsx
@@ -4,6 +4,6 @@ import { Layout } from 'antd';
 const { Content } = Layout;
 
 export default styled(Content)`
-  padding: ${(props) => props.theme.desktopLayout.paddingVertical}
-    ${(props) => props.theme.desktopLayout.paddingHorizontal};
+  padding: ${(props) => props.theme.desktopLayout.paddingVertical};
+  ${(props) => props.theme.desktopLayout.paddingHorizontal};
 `;
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import MainLayout from '../../components/layouts/MainLayout';
 import { Card } from 'antd';
-import { CovidChartCard } from '../Dashboard/CovidChart/CovidChartCard';
+import { CovidChartCard } from './CovidChart/CovidChartCard';
+import { ActivityChartCard } from './ActivityChart/ActivityChartCard';
 
 const Dashboard: React.FC = () => {
   return (
     <MainLayout>
       <Card>SOME COMPONENT</Card>
       <CovidChartCard />
+      <ActivityChartCard />
     </MainLayout>
   );
 };
