3cf133ee25dc860f89984e0258ab33192202e44d
armistrice <armistrice@gmail.com>
2021-07-30 23:35:10 +0300

    feat(card): added box-shadow

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -20,6 +20,7 @@ export const Card: React.FC<CardProps> = ({ type = 'medium', padding, children, 
 const CardStyled = styled(AntCard)<CardProps>`
   min-height: 18.5rem;
   height: ${(props) => (props.type === 'small' ? '18.5rem' : props.type === 'medium' ? '23.5rem' : '28.5rem')};
+  box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.07);
 
   & .ant-card-body {
     padding: ${(props) => props.padding};
