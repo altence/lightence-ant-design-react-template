432dfd7182297e22a28288bb164a33d39c7271a2
armistrice <armistrice@gmail.com>
2021-07-30 22:53:19 +0300

    feat(card): added types for card

--- src/components/common/Card.tsx
+++ src/components/common/Card.tsx
@@ -5,18 +5,22 @@ import styled from 'styled-components';
 interface CardProps {
   padding?: string | number;
   children: React.ReactNode;
+  type?: 'small' | 'medium' | 'large';
   [key: string]: React.ReactNode;
 }
 
-export const Card: React.FC<CardProps> = ({ padding, children, ...props }) => {
+export const Card: React.FC<CardProps> = ({ type = 'medium', padding, children, ...props }) => {
   return (
-    <CardStyled padding={padding} {...props}>
+    <CardStyled type={type} padding={padding} {...props}>
       {children}
     </CardStyled>
   );
 };
 
 const CardStyled = styled(AntCard)<CardProps>`
+  min-height: 18.5rem;
+  height: ${(props) => (props.type === 'small' ? '18.5rem' : props.type === 'medium' ? '23.5rem' : '28.5rem')};
+
   & .ant-card-body {
     padding: ${(props) => props.padding};
   }
