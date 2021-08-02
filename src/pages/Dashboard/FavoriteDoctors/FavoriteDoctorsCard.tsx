73ed4f74b3a3ab944d3d8d33ed4594255a2ae221
armistrice <armistrice@gmail.com>
2021-08-03 01:04:53 +0300

    feat(favorite-doctors): added favorite doctors card to dashboard

--- src/pages/Dashboard/FavoriteDoctors/FavoriteDoctorsCard.tsx
+++ src/pages/Dashboard/FavoriteDoctors/FavoriteDoctorsCard.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import { SwiperSlide } from 'swiper/react';
 import { Card } from '../../../components/common/Card';
+import { Slider } from '../../../components/common/Slider';
+import { favoriteDoctors } from '../../../constants/favoriteDoctors';
+import { DoctorCard } from './DoctorCard';
 
 export const FavoriteDoctorsCard: React.FC = () => {
-  return <Card title="Favorite doctors">Favorite doctors card</Card>;
+  return (
+    <Card title="Favorite doctors">
+      <Slider spaceBetween={24} slidesPerView={3}>
+        {favoriteDoctors.map((doctor) => (
+          <SwiperSlide key={doctor.id}>
+            <DoctorCard
+              imgUrl={doctor.imgUrl}
+              name={doctor.name}
+              specifity={doctor.specifity}
+              rating={doctor.rating}
+              visit={doctor.visit}
+            />
+          </SwiperSlide>
+        ))}
+      </Slider>
+    </Card>
+  );
 };
