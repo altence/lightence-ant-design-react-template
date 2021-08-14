3e1f5188aea40cf6fed076d47a3ab8a6ae46028c
armistrice <armistrice@gmail.com>
2021-08-14 23:58:16 +0300

    feat(dashboard): made adaptive dashboard layout

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -17,64 +17,100 @@ import { statisticsData } from '../../constants/statisticsData';
 import { media } from '../../styles/theme';
 
 const Dashboard: React.FC = () => {
-  const isPC = useMediaQuery({ query: media.xl });
+  const isDesktop = useMediaQuery({ query: media.xl });
+  const isTablet = useMediaQuery({ query: media.md });
+  const isMobile = !isDesktop && !isTablet;
 
   return (
     <MainLayout>
       <Row justify="space-between" gutter={[10, 10]}>
-        {statisticsData.map((st, index) => (
-          <Col key={st.id} order={(!isPC && index + 1) || 0} xs={12} xl={6}>
-            <StatisticsCard
-              title={st.title}
-              icon={st.icon}
-              value={st.value}
-              percent={st.percent}
-              isDowngrade={st.isDowngrade}
-              color={st.color}
-              chartColor={st.chartColor}
-            />
-          </Col>
-        ))}
+        {isMobile && (
+          <>
+            {statisticsData.map((st) => (
+              <Col key={st.id} xs={12}>
+                <StatisticsCard
+                  title={st.title}
+                  icon={st.icon}
+                  value={st.value}
+                  percent={st.percent}
+                  isDowngrade={st.isDowngrade}
+                  color={st.color}
+                  chartColor={st.chartColor}
+                />
+              </Col>
+            ))}
 
-        {isPC && (
-          <Col order={0} xl={9}>
+            <Col xs={24} xl={15}>
+              <ScreeningsCard />
+            </Col>
+          </>
+        )}
+
+        {isTablet && (
+          <>
+            <Col order={1} md={16}>
+              <Row justify="space-between" gutter={[10, 10]}>
+                {statisticsData.map((st) => (
+                  <Col key={st.id} md={6}>
+                    <StatisticsCard
+                      title={st.title}
+                      icon={st.icon}
+                      value={st.value}
+                      percent={st.percent}
+                      isDowngrade={st.isDowngrade}
+                      color={st.color}
+                      chartColor={st.chartColor}
+                    />
+                  </Col>
+                ))}
+                <Col md={24}>
+                  <ScreeningsCard />
+                </Col>
+              </Row>
+            </Col>
+            <Col order={2} md={8}>
+              <BloodScreeningCard />
+            </Col>
+          </>
+        )}
+
+        {isTablet && (
+          <Col order={3} md={12} xl={9}>
             <MapCard />
           </Col>
         )}
 
-        <Col order={(!isPC && 5) || 0} xs={24} xl={15}>
-          <ScreeningsCard />
-        </Col>
-
-        <Col order={(!isPC && 7) || 0} xs={24} xl={15}>
-          <TreatmentCard />
-        </Col>
-
-        <Col order={(!isPC && 6) || 0} xs={24} xl={9}>
+        <Col order={(isTablet && 4) || 0} xs={24} md={12} xl={9}>
           <ActivityCard />
         </Col>
 
-        <Col order={(!isPC && 12) || 0} xs={24} xl={12}>
-          <CovidCard />
+        <Col order={(isTablet && 5) || 0} xs={24} xl={15}>
+          <TreatmentCard />
         </Col>
 
-        <Col order={(!isPC && 8) || 0} xs={24} xl={12}>
+        <Col order={(isTablet && 6) || 0} xs={24} xl={12}>
           <HealthCard />
         </Col>
 
-        <Col order={(!isPC && 11) || 0} xs={24} xl={9}>
-          <FavouritesDoctorsCard />
-        </Col>
-
-        <Col order={(!isPC && 9) || 0} xs={24} xl={9}>
+        <Col order={(isTablet && 7) || 0} xs={24} md={12} xl={9}>
           <PatientTimelineCard />
         </Col>
 
-        <Col order={(!isPC && 10) || 0} xs={24} xl={6}>
-          <BloodScreeningCard />
+        {isMobile && (
+          <Col xs={24}>
+            <BloodScreeningCard />
+          </Col>
+        )}
+
+        <Col order={(isTablet && 9) || 0} xs={24} xl={9}>
+          <FavouritesDoctorsCard />
         </Col>
 
-        <Col order={(!isPC && 13) || 0} span={24}>
+        <Col order={(isTablet && 8) || 0} xs={24} md={12} xl={12}>
+          <CovidCard />
+        </Col>
+
+        <Col order={(isTablet && 10) || 0} span={24}>
           <PiecesOfAdviceCard />
         </Col>
       </Row>
