1f95b380f8c95450eb46e0a948a7084136295379
armistrice <armistrice@gmail.com>
2021-08-17 00:52:23 +0300

    feat(favourites-doctors): made big screen version

--- src/components/dashboard/FavouritesDoctorsCard/DoctorCard/DoctorCard.styles.ts
+++ src/components/dashboard/FavouritesDoctorsCard/DoctorCard/DoctorCard.styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Avatar as AntAvatar, Typography, Rate } from 'antd';
+import { media } from '../../../../styles/theme';
 
 export const Wrapper = styled.div`
   margin: 0 auto;
@@ -12,6 +13,10 @@ export const Wrapper = styled.div`
 
   background-color: ${(props) => props.theme.colors.secondary};
 
+  @media only screen and ${media.xxl} {
+    max-width: 9.5rem;
+  }
+
   &:hover {
     background-color: ${(props) => props.theme.colors.primaryLight};
   }
@@ -22,12 +27,20 @@ export const Title = styled(Typography.Text)`
   font-size: 0.625rem;
 
   color: ${(props) => props.theme.colors.primary};
+
+  @media only screen and ${media.xxl} {
+    font-size: 0.75rem;
+  }
 `;
 
 export const Text = styled(Typography.Text)`
   display: block;
   font-weight: 500;
   font-size: 0.625rem;
+
+  @media only screen and ${media.xxl} {
+    font-size: 1rem;
+  }
 `;
 
 export const Avatar = styled(AntAvatar)`
@@ -37,6 +50,11 @@ export const Avatar = styled(AntAvatar)`
   width: 100%;
   height: 100%;
   max-width: 6.25rem;
+
+  @media only screen and ${media.xxl} {
+    max-width: 8.75rem;
+    margin-bottom: 0.625rem;
+  }
 `;
 
 export const RatingWrapper = styled.div`
@@ -45,24 +63,53 @@ export const RatingWrapper = styled.div`
   align-items: center;
   margin-bottom: 0.125rem;
 
+  @media only screen and ${media.xxl} {
+    margin-bottom: 1rem;
+  }
+
   & ${Text} {
     margin-left: 0.3rem;
+
+    @media only screen and ${media.xxl} {
+      margin-left: 0.5rem;
+    }
   }
 `;
 
 export const Rating = styled(Rate)`
+  @media only screen and ${media.xxl} {
+    font-size: 1rem;
+  }
+
   & .ant-rate-star:not(:last-child) {
     margin-right: 0.25rem;
+
+    @media only screen and ${media.xxl} {
+      margin-left: 0.3rem;
+    }
   }
 `;
 
 export const InfoWrapper = styled.div`
   display: flex;
   flex-direction: column;
+
+  @media only screen and ${media.xxl} {
+    & ${Text} {
+      margin-bottom: 0.625rem;
+    }
+  }
 `;
 
 export const VisitWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+
+  @media only screen and ${media.xxl} {
+    & ${Text} {
+      font-size: 0.75rem;
+      margin-bottom: 0;
+    }
+  }
 `;
