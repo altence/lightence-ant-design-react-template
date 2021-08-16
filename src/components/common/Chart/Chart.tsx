5f7f242345d0a2ecc2945782d644ef447ee6fe97
armistrice <armistrice@gmail.com>
2021-08-16 21:59:43 +0300

    feat(charts): added media query for big screen

--- src/components/common/Chart/Chart.tsx
+++ src/components/common/Chart/Chart.tsx
@@ -11,10 +11,14 @@ interface ChartProps extends S.ChartProps {
 export const Chart: React.FC<ChartProps> = ({ option, width, height }) => {
   const isTablet = useMediaQuery({ query: media.md });
   const isDesktop = useMediaQuery({ query: media.xl });
+  const isBigScreen = useMediaQuery({ query: media.xxl });
 
   const chartHeight = height
     ? height
-    : (isDesktop && '250px') || (isTablet && '200px') || (!isDesktop && !isTablet && '200px') || '100%';
+    : (isBigScreen && '350px') ||
+      (isDesktop && '250px') ||
+      (isTablet && '200px') ||
+      (!isDesktop && !isTablet && '200px');
 
   return <S.Chart option={option} width={width} height={chartHeight} />;
 };
