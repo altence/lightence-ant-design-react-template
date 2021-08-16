bbc77d81e7e65c4c9f4b8370c4fe850424f82d6f
armistrice <armistrice@gmail.com>
2021-08-16 16:13:57 +0300

    feat(map): made desktop version

--- src/components/dashboard/MapCard/Map/Map.tsx
+++ src/components/dashboard/MapCard/Map/Map.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import L from 'leaflet';
 import { Marker, TileLayer } from 'react-leaflet';
+import { useMediaQuery } from 'react-responsive';
 import { Popup } from '../Popup/Popup';
 import { doctorsData, polyclinicData } from '../../../../constants/mapData';
 import markerDoctor from '../../../../assets/icons/marker-doctor.svg';
 import markerPolyclinic from '../../../../assets/icons/marker-polyclinic.svg';
+import { media } from '../../../../styles/theme';
 import * as S from './Map.styles';
 
 export const Map: React.FC = () => {
+  const isDesktop = useMediaQuery({ query: media.xl });
+
+  const MarkerDoctor = new L.Icon({
+    iconUrl: markerDoctor,
+    iconRetinaUrl: markerDoctor,
+    popupAnchor: [-0, -0],
+    iconSize: (isDesktop && [50, 70]) || [30, 50],
+  });
+
+  const MarkerPolyclinic = new L.Icon({
+    iconUrl: markerPolyclinic,
+    iconRetinaUrl: markerPolyclinic,
+    popupAnchor: [-0, -0],
+    iconSize: (isDesktop && [50, 70]) || [30, 50],
+  });
+
   return (
     <S.Map center={[42.343574, -83.046792]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
@@ -33,17 +51,3 @@ export const Map: React.FC = () => {
     </S.Map>
   );
 };
-
-const MarkerDoctor = new L.Icon({
-  iconUrl: markerDoctor,
-  iconRetinaUrl: markerDoctor,
-  popupAnchor: [-0, -0],
-  iconSize: [30, 50],
-});
-
-const MarkerPolyclinic = new L.Icon({
-  iconUrl: markerPolyclinic,
-  iconRetinaUrl: markerPolyclinic,
-  popupAnchor: [-0, -0],
-  iconSize: [30, 50],
-});
