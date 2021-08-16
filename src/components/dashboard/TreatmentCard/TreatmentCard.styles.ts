fd42be5818f96b3849b645107dc3139f6214dd83
armistrice <armistrice@gmail.com>
2021-08-17 00:41:26 +0300

    feat(treatment): made big screen version

--- src/components/dashboard/TreatmentCard/TreatmentCard.styles.ts
+++ src/components/dashboard/TreatmentCard/TreatmentCard.styles.ts
@@ -1,8 +1,13 @@
 import styled from 'styled-components';
+import { media } from '../../../styles/theme';
 
 export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: 300px;
+
+  @media only screen and ${media.xxl} {
+    height: 350px;
+  }
 `;
