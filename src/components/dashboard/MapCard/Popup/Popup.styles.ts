f69a436b913548220160bb379073447d4fc3837f
armistrice <armistrice@gmail.com>
2021-08-17 00:10:54 +0300

    feat(map): made big screen version

--- src/components/dashboard/MapCard/Popup/Popup.styles.ts
+++ src/components/dashboard/MapCard/Popup/Popup.styles.ts
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 import { Typography, Avatar as AntAvatar, Rate } from 'antd';
 import { Popup as LPopup } from 'react-leaflet';
+import { media } from '../../../../styles/theme';
 
 export const Wrapper = styled(LPopup)`
   font-size: 0.625rem;
+
+  @media only screen and ${media.xxl} {
+    font-size: 0.75rem;
+  }
 
   & .leaflet-popup-content-wrapper {
     border-radius: ${(props) => props.theme.border.radius};
@@ -27,6 +32,10 @@ export const Avatar = styled(AntAvatar)`
   min-width: 2.5rem;
   height: 100%;
   margin-right: 0.625rem;
+
+  @media only screen and ${media.xxl} {
+    min-width: 3.12rem;
+  }
 `;
 
 export const InfoWrapper = styled.div`
@@ -57,4 +66,8 @@ export const Rating = styled(Rate)`
   margin-right: 0.375rem;
 
   color: ${(props) => props.theme.colors.accent};
+
+  @media only screen and ${media.xxl} {
+    font-size: 1rem;
+  }
 `;
