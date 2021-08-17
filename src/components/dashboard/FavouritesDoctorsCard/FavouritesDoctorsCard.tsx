6f29a3020a9207a6cb83a93cb66ac2243cf30f67
armistrice <armistrice@gmail.com>
2021-08-17 12:48:11 +0300

    feat(favourites-doctors): hooked up new db

--- src/components/dashboard/FavouritesDoctorsCard/FavouritesDoctorsCard.tsx
+++ src/components/dashboard/FavouritesDoctorsCard/FavouritesDoctorsCard.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { SwiperSlide } from 'swiper/react';
+import { useMediaQuery } from 'react-responsive';
 import { Card } from '../../../components/common/Card/Card';
 import { Slider } from '../../../components/common/Slider/Slider';
-import { favouritesDoctorsData } from '../../../constants/favouritesDoctorsData';
 import { DoctorCard } from './DoctorCard/DoctorCard';
+import { doctorsData } from '../../../constants/doctorsData';
+import { media } from '../../../styles/theme';
 
 export const FavouritesDoctorsCard: React.FC = () => {
+  const isBigScreen = useMediaQuery({ query: media.xxl });
+
   return (
-    <Card title="Favorite doctors" padding="0.625rem">
+    <Card title="Favorite doctors" padding={(isBigScreen && '1rem') || '0.625rem'}>
       <Slider>
-        {favouritesDoctorsData.map((doctor) => (
+        {doctorsData.map((doctor) => (
           <SwiperSlide key={doctor.id}>
             <DoctorCard
               imgUrl={doctor.imgUrl}
               name={doctor.name}
               specifity={doctor.specifity}
               rating={doctor.rating}
-              visit={doctor.visit}
+              lastVisit={doctor.lastVisit}
             />
           </SwiperSlide>
         ))}
