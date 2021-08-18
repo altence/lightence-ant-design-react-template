569817bad022ae0585c47515618e91f35d9bb657
armistrice <armistrice@gmail.com>
2021-08-18 17:26:17 +0300

    feat(screenings): made active item logic

--- src/components/dashboard/ScreeningsCard/ScreeningsCollapse/ScreeningsCollapse.tsx
+++ src/components/dashboard/ScreeningsCard/ScreeningsCollapse/ScreeningsCollapse.tsx
@@ -1,32 +1,66 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import { ArrowLeftOutlined, ArrowRightOutlined, CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
 import { doctorsData } from '../../../../constants/doctorsData';
 import * as S from './ScreeningsCollapse.styles';
 
-const getFriendsData = () => {
-  const result = [];
-
-  for (let i = 0; i <= doctorsData.length && i < 6; i++) {
-    if (i === 1 || i === 5) {
-      result.push({
-        isDowngrade: true,
-        value: Math.floor(1 + Math.random() * 99),
-        ...doctorsData[i],
-      });
-    } else {
-      result.push({
-        isDowngrade: false,
-        value: Math.floor(1 + Math.random() * 99),
-        ...doctorsData[i],
-      });
-    }
-  }
-
-  return result;
-};
+interface ScreeningsItemState {
+  firstItem: number;
+  secondItem: number;
+  isFirstClick: boolean;
+}
 
 export const ScreeningsCollapse: React.FC = () => {
   const [isCollapsed, setCollapsed] = useState(true);
+
+  const [activeItems, setActiveItems] = useState<ScreeningsItemState>({
+    firstItem: 1,
+    secondItem: 4,
+    isFirstClick: true,
+  });
+
+  const handleClickItem = (mode: number) => () => {
+    setActiveItems((prev) => {
+      if (prev.isFirstClick && prev.secondItem !== mode) {
+        return {
+          ...prev,
+          isFirstClick: !prev.isFirstClick,
+          firstItem: mode,
+        };
+      } else if (prev.firstItem !== mode) {
+        return {
+          ...prev,
+          isFirstClick: !prev.isFirstClick,
+          secondItem: mode,
+        };
+      } else {
+        return {
+          ...prev,
+        };
+      }
+    });
+  };
+
+  const data = useMemo(() => {
+    const result = [];
+
+    for (let i = 0; i <= doctorsData.length && i < 6; i++) {
+      if (i === 1 || i === 5) {
+        result.push({
+          isDowngrade: true,
+          value: Math.floor(1 + Math.random() * 99),
+          ...doctorsData[i],
+        });
+      } else {
+        result.push({
+          isDowngrade: false,
+          value: Math.floor(1 + Math.random() * 99),
+          ...doctorsData[i],
+        });
+      }
+    }
+
+    return result;
+  }, [doctorsData]);
 
   return (
     <S.Wrapper isCollapsed={isCollapsed}>
@@ -37,8 +71,15 @@ export const ScreeningsCollapse: React.FC = () => {
         </S.ArrowBtn>
       </S.HeaderWrapper>
       <S.List>
-        {getFriendsData().map((friend) => (
-          <S.ListItem key={friend.id} isCollapsed={isCollapsed} isDowngrade={friend.isDowngrade}>
+        {data.map((friend, index) => (
+          <S.ListItem
+            key={friend.id}
+            isCollapsed={isCollapsed}
+            isDowngrade={friend.isDowngrade}
+            isFirstActive={activeItems.firstItem === index}
+            isSecondActive={activeItems.secondItem === index}
+            onClick={handleClickItem(index)}
+          >
             <S.Avatar src={friend.imgUrl} shape="square" />
             {!isCollapsed && (
               <>
