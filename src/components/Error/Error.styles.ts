016890f49db026e31902cd0d5eb6c9f30c1c2ed1
armistrice <armistrice@gmail.com>
2021-08-25 23:08:35 +0300

    feat(app): add errors 404 and 500

---
+++ src/components/Error/Error.styles.ts
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+import { Button, Typography, Image as AntImage } from 'antd';
+import theme from '../../styles/theme';
+
+export const Wrapper = styled.div`
+  font-family: Poppins, sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 3.75rem 1.25rem;
+
+  background-color: ${(props) => props.theme.colors.secondary};
+
+  @media only screen and ${theme.media.md} {
+    padding: 2.5rem 6.25rem 6.25rem;
+  }
+
+  @media only screen and ${theme.media.xl} {
+    flex-direction: row-reverse;
+    justify-content: center;
+    padding: 12.5rem 3.5rem;
+  }
+`;
+
+export const Image = styled(AntImage)`
+  margin-bottom: 4rem;
+
+  @media only screen and ${theme.media.xxl} {
+    margin-bottom: 0;
+  }
+`;
+
+export const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media only screen and ${theme.media.xl} {
+    margin-right: 7.5rem;
+  }
+`;
+
+export const Title = styled(Typography.Text)`
+  font-size: 2.25rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+
+  color: ${(props) => props.theme.colors.basicDark};
+
+  @media only screen and ${theme.media.md} {
+    font-size: 3rem;
+    margin-bottom: 1.75rem;
+  }
+
+  @media only screen and ${theme.media.xl} {
+    font-size: 4rem;
+    margin-bottom: 2.25rem;
+  }
+`;
+
+export const Text = styled(Title)`
+  font-size: 0.875rem;
+  margin-bottom: 1.25rem;
+
+  @media only screen and ${theme.media.md} {
+    font-size: 1.12rem;
+    margin-bottom: 1.45rem;
+  }
+
+  @media only screen and ${theme.media.xl} {
+    font-size: 1.5rem;
+    margin-bottom: 1.8rem;
+  }
+`;
+
+export const Btn = styled(Button)`
+  width: 100%;
+
+  @media only screen and ${theme.media.md} {
+    font-size: 1rem;
+  }
+`;
