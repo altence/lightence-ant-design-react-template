9865bf70bf5223b287fe4d6ff55f894135c434ef
armistrice <armistrice@gmail.com>
2021-08-26 13:03:30 +0300

    fix(error-pages): make more abstract error component and add links to sider

--- src/pages/Error500.tsx
+++ src/pages/Error500.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Error } from '../components/Error/Error';
 import MainLayout from '../components/layouts/MainLayout';
+import error500 from '../assets/images/error500.svg';
 
 const Error404: React.FC = () => {
   return (
     <MainLayout>
-      <Error error={500} />
+      <Error img={error500} msg="Internal server error" btnText="Try again" />
     </MainLayout>
   );
 };
