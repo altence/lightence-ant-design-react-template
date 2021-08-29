9d61eb5bc4e9d9229bb78f68f99f292ce5fd6273
armistrice <armistrice@gmail.com>
2021-08-29 21:28:36 +0300

    fix(header): removed unnecessary file

--- src/constants/userData.ts
+++
@@ -1,9 +0,0 @@
-interface User {
-  name: string;
-  imgUrl: string;
-}
-
-export const userData: User = {
-  name: 'Alexandr Y.',
-  imgUrl: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1629465123/Ellipse_59_vls5ji.jpg',
-};
