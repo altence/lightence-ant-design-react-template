319be997c18d62efc26170e8d7834161ec28a024
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-10-01 19:14:25 +0300

    refactor(theme): refactor manual theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { ThemeSwitchContext } from './context/ThemeSwitchContext';
-import { getTheme } from './helpers/getTheme';
-import { initialNightTime } from './constants/initialNightTime';
-import { AppDate, Dates } from './constants/Dates';
-import theme, { darkTheme } from './styles/theme';
+import { ThemeContext } from './context/ThemeContext';
+import lightTheme, { darkTheme } from './styles/theme';
 import { routes } from './routes/routes';
 import GlobalStyle from './styles/GlobalStyle';
 import 'typeface-poppins';
 import 'typeface-montserrat';
+import { useTheme } from './hooks/useTheme';
 
 const App: React.FC = () => {
-  const localIsAutoChangeEnabled = localStorage.getItem('autoChange');
-  const localTheme = localStorage.getItem('theme');
-  const localNightTime = localStorage.getItem('nightTime');
-
-  const [currentTheme, setCurrentTheme] = useState(localTheme || 'light');
-  const [isAutoChangeEnabled, setAutoChangeEnabled] = useState(
-    (!localIsAutoChangeEnabled && true) || JSON.parse(localIsAutoChangeEnabled),
-  );
-  const [nightTime, setNightTime] = useState<AppDate[]>(
-    (localNightTime && JSON.parse(localNightTime).map((el: string) => Dates.getDate(el))) || initialNightTime,
-  );
-  const [now, setNow] = useState(Dates.getToday());
-
-  const handleThemeSwitch = (theme: string) => {
-    setCurrentTheme(theme);
-  };
-
-  useEffect(() => {
-    const id = setInterval(() => {
-      setNow(Dates.getToday());
-    }, 60000);
-
-    return () => {
-      clearInterval(id);
-    };
-  }, []);
-
-  useEffect(() => {
-    if (isAutoChangeEnabled) {
-      getTheme(nightTime[0], nightTime[1], now) === 'light' ? setCurrentTheme('light') : setCurrentTheme('dark');
-    }
-  }, [now, nightTime, isAutoChangeEnabled]);
+  const { theme, setTheme } = useTheme();
 
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={currentTheme === 'light' ? theme : darkTheme}>
-        <ThemeSwitchContext.Provider
-          value={{
-            currentTheme,
-            changeTheme: handleThemeSwitch,
-            changeNightTime: setNightTime,
-            isAutoSwitchEnabled: isAutoChangeEnabled,
-            changeAutoSwitchEnabled: setAutoChangeEnabled,
-          }}
-        >
+      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <ThemeContext.Provider value={{ theme, setTheme }}>
           <BrowserRouter>
             <Switch>
               {routes.map((route, index) => (
@@ -65,7 +24,7 @@ const App: React.FC = () => {
               ))}
             </Switch>
           </BrowserRouter>
-        </ThemeSwitchContext.Provider>
+        </ThemeContext.Provider>
       </ThemeProvider>
     </>
   );
--- src/components/Header/SettingsDropdown/ThemePicker/ThemePicker.tsx
+++ src/components/Header/SettingsDropdown/ThemePicker/ThemePicker.tsx
@@ -1,42 +1,26 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ThemeSwitchContext } from '../../../../context/ThemeSwitchContext';
+import { ThemeContext } from '../../../../context/ThemeContext';
 import { Switch } from 'antd';
 import { AutoThemePicker } from './AutoThemePicker/AutoThemePicker';
 import * as S from './ThemePicker.styles';
 
 export const ThemePicker: React.FC = () => {
-  const { currentTheme, changeTheme } = useContext(ThemeSwitchContext);
+  const { theme, setTheme } = useContext(ThemeContext);
 
-  const [isChecked, setChecked] = useState(currentTheme === 'dark' ? true : false);
+  const onChange = (value: boolean) => {
+    setTheme(value ? 'dark' : 'light');
+  };
 
   const { t } = useTranslation();
-
-  useEffect(() => {
-    if (currentTheme === 'dark') {
-      setChecked(true);
-    } else {
-      setChecked(false);
-    }
-  });
-
-  const handleSwitch = (value: boolean) => {
-    if (value && currentTheme !== 'dark') {
-      changeTheme('dark');
-      localStorage.setItem('theme', 'dark');
-    } else if (!value && currentTheme !== 'light') {
-      changeTheme('light');
-      localStorage.setItem('theme', 'light');
-    }
-  };
 
   return (
     <S.SwitchWrapper>
       <Switch
-        checked={isChecked}
+        defaultChecked={theme === 'dark'}
         checkedChildren={t('common.darkTheme')}
         unCheckedChildren={t('common.lightTheme')}
-        onChange={handleSwitch}
+        onChange={onChange}
       />
       <AutoThemePicker />
     </S.SwitchWrapper>
