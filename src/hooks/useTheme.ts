e6119251e184dcc94b26aa7739bf80733119c868
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-10-01 19:18:13 +0300

    feat(theme): save current theme into local storage

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -5,10 +5,18 @@ interface UseThemeReturnVal {
   setTheme: (theme: string) => void;
   isNightMode: boolean;
 }
+
+const selectedTheme = localStorage.getItem('theme');
+
 export const useTheme = (): UseThemeReturnVal => {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState(selectedTheme || 'light');
+
+  const selectTheme = (theme: string): void => {
+    setTheme(theme);
+    localStorage.setItem('theme', theme);
+  };
 
   const [isNightMode] = useState(false);
 
-  return { theme, setTheme, isNightMode };
+  return { theme, setTheme: selectTheme, isNightMode };
 };
