2ddd952756348b29d0ce9b8acca9f9d985da400b
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-10-04 19:40:24 +0300

    feat(i18n): updating app lang also changes date-time localization

--- src/components/Header/SettingsDropdown/LanguagePicker/LanguagePicker.tsx
+++ src/components/Header/SettingsDropdown/LanguagePicker/LanguagePicker.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Radio, RadioChangeEvent } from 'antd';
 import { useTranslation } from 'react-i18next';
+import { Dates } from '../../../../constants/Dates';
 
 export const LanguagePicker: React.FC = () => {
   const { i18n } = useTranslation();
 
-  const handleChange = (e: RadioChangeEvent) => {
-    i18n.changeLanguage(e.target.value);
+  const handleChange = async (e: RadioChangeEvent) => {
+    const locale = e.target.value;
+
+    Dates.setLocale(locale);
+    await i18n.changeLanguage(locale);
   };
 
   return (
