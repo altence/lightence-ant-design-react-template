b73369db86c503216b815913384de0aaad7e6f0f
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-10-05 13:33:31 +0300

    refactor(theme): remove redundant files

--- src/helpers/getNumberFromTime.ts
+++
@@ -1,7 +0,0 @@
-import { AppDate, Dates } from '../constants/Dates';
-
-export const getNumberFromTime = (date: AppDate): number => {
-  const formattedDate = Dates.format(date, 'HH:mm');
-
-  return parseInt(formattedDate.replace(':', ''), 0);
-};
