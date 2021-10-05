52429ecad91b2ad5170855e30b3b3b319fcf9ec9
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-10-05 14:59:46 +0300

    fix(app): fix some conflicts after rebasing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { ConfigProvider } from 'antd';
 import deDe from 'antd/lib/locale/de_DE';
 import enUS from 'antd/lib/locale/en_US';
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { ThemeContext } from './context/ThemeContext';
 import { NightModeContext } from './context/NightModeContext';
-import lightTheme, { darkTheme } from './styles/theme';
+import lightTheme from './styles/theme';
 import { routes } from './routes/routes';
 import GlobalStyle from './styles/GlobalStyle';
 import 'typeface-poppins';
@@ -24,10 +24,10 @@ const App: React.FC = () => {
   return (
     <>
       <GlobalStyle />
-      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <ThemeProvider theme={theme === 'light' ? lightTheme : lightTheme}>
         <ThemeContext.Provider value={{ theme, setTheme }}>
           <NightModeContext.Provider value={{ isNightMode, setNightMode, nightTime, setNightTime }}>
-            <ConfigProvider locale={currentLocale === 'en' ? enUS : deDe}>
+            <ConfigProvider locale={currentLanguage === 'en' ? enUS : deDe}>
               <BrowserRouter>
                 <Switch>
                   {routes.map((route, index) => (
