aa60b3fb8d6f9fc1defc0665c8e779669a6e5a89
armistrice <egor.leineveber@altence.co>
2021-10-06 16:22:22 +0300

    feat(profile-page): main card tablet version

--- src/components/profile/ProfileCard/ProfileCard.styles.ts
+++ src/components/profile/ProfileCard/ProfileCard.styles.ts
@@ -1,6 +1,16 @@
 import styled from 'styled-components';
 import { Button } from 'antd';
 
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  column-gap: 2rem;
+
+  & > div {
+    width: 50%;
+  }
+`;
+
 export const Btn = styled(Button)`
   color: inherit;
   font-size: 1rem;
--- src/components/profile/ProfileCard/ProfileCard.tsx
+++ src/components/profile/ProfileCard/ProfileCard.tsx
@@ -8,10 +8,15 @@ import { PageTitle } from 'components/common/PageTitle/PageTitle';
 import { ProfileFormNav } from './ProfileFormNav/ProfileFormNav';
 import { getUser, User } from 'api/users.api';
 import * as S from './ProfileCard.styles';
+import { useMediaQuery } from 'react-responsive';
+import theme from 'styles/theme';
+import { Col, Row } from 'antd';
 
 export const ProfileCard: React.FC = () => {
-  const [currentMenu, setCurrentMenu] = useState('main');
+  const isMobile = useMediaQuery({ query: theme.media.xs });
+  const isTablet = useMediaQuery({ query: theme.media.md });
 
+  const [currentMenu, setCurrentMenu] = useState((isTablet && 'info') || 'main');
   const [profileData, setProfileData] = useState<User>();
 
   const { t } = useTranslation();
@@ -23,7 +28,7 @@ export const ProfileCard: React.FC = () => {
   return profileData ? (
     <>
       <PageTitle>
-        {currentMenu === 'main' ? (
+        {isTablet || currentMenu === 'main' ? (
           t('profile.title')
         ) : (
           <S.Btn icon={<LeftOutlined />} type="text" onClick={() => setCurrentMenu('main')}>
@@ -32,13 +37,31 @@ export const ProfileCard: React.FC = () => {
         )}
       </PageTitle>
 
-      {currentMenu === 'main' ? (
-        <Card padding={30}>
-          <ProfileInfo profileData={profileData} />
-          <ProfileNav setCurrentMenu={setCurrentMenu} />
-        </Card>
-      ) : (
-        <ProfileFormNav menu={currentMenu} />
+      {!isTablet &&
+        (currentMenu === 'main' ? (
+          <Card padding={30}>
+            <ProfileInfo profileData={profileData} />
+            <ProfileNav setCurrentMenu={setCurrentMenu} />
+          </Card>
+        ) : (
+          <ProfileFormNav menu={currentMenu} />
+        ))}
+
+      {isTablet && (
+        <Row gutter={[0, 30]}>
+          <Col span={24}>
+            <Card padding={30}>
+              <S.Wrapper>
+                <ProfileInfo profileData={profileData} />
+                <ProfileNav setCurrentMenu={setCurrentMenu} />
+              </S.Wrapper>
+            </Card>
+          </Col>
+
+          <Col span={24}>
+            <ProfileFormNav menu={currentMenu} />
+          </Col>
+        </Row>
       )}
     </>
   ) : null;
