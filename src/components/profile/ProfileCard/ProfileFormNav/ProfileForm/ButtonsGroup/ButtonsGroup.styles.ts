972cfec1ceb232bbc40a5270fe4f2eff1453096d
armistrice <egor.leineveber@altence.co>
2021-10-07 23:02:27 +0300

    feat(profile-page): change cancel/save buttons width

--- src/components/profile/ProfileCard/ProfileFormNav/ProfileForm/ButtonsGroup/ButtonsGroup.styles.ts
+++ src/components/profile/ProfileCard/ProfileFormNav/ProfileForm/ButtonsGroup/ButtonsGroup.styles.ts
@@ -4,6 +4,10 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
 
+  @media only screen and ${(props) => props.theme.media.md} {
+    width: calc(50% - 10px);
+  }
+
   & > button {
     width: calc(50% - 0.5rem);
   }
