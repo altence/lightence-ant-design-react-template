1623223ee39391be6b457f22e740c910f1f680d1
armistrice <egor.leineveber@altence.co>
2021-10-08 15:27:26 +0300

    fix(profile-page): use common button

--- src/components/profile/ProfileCard/ProfileNav/ProfileNav.tsx
+++ src/components/profile/ProfileCard/ProfileNav/ProfileNav.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import { useMediaQuery } from 'react-responsive';
+import React, { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { profileNavData } from 'constants/profileNavData';
-import theme from 'styles/theme';
 import * as S from './ProfileNav.styles';
 
 interface ProfileNavProps {
@@ -12,19 +10,17 @@ interface ProfileNavProps {
 export const ProfileNav: React.FC<ProfileNavProps> = ({ setCurrentMenu }) => {
   const { t } = useTranslation();
 
-  const isDesktop = useMediaQuery({ query: theme.media.xl });
+  const onClickBtn = useCallback(
+    (href) => () => {
+      setCurrentMenu(href);
+    },
+    [setCurrentMenu],
+  );
 
   return (
     <S.Wrapper>
       {profileNavData.map((item) => (
-        <S.Btn
-          size={(isDesktop && 'large') || 'middle'}
-          key={item.id}
-          icon={<item.Icon />}
-          type="text"
-          color={item.color}
-          onClick={() => setCurrentMenu(item.href)}
-        >
+        <S.Btn key={item.id} icon={<item.Icon />} type="text" color={item.color} onClick={onClickBtn(item.href)}>
           {t(item.name)}
         </S.Btn>
       ))}
