c332392179e955da8c1d8f804f70cd843aff6cc0
armistrice <egor.leineveber@altence.co>
2021-10-11 16:09:37 +0300

    fix(social-links): update icons

---
+++ src/components/common/icons/LinkedinIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const LinkedinIcon: React.FC = () => (
+  <svg width="1em" height="1em" viewBox="0 0 18 17" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M3.75 1.84783C3.75 2.88261 2.925 3.69565 1.875 3.69565C0.825 3.69565 0 2.88261 0 1.84783C0 0.813043 0.825 0 1.875 0C2.925 0 3.75 0.813043 3.75 1.84783ZM3.75 5.17391H0V17H3.75V5.17391ZM9.75 5.17391H6V17H9.75V10.7913C9.75 7.31739 14.25 7.02174 14.25 10.7913V17H18V9.53478C18 3.69565 11.325 3.91739 9.75 6.8V5.17391Z"
+      fill="currentColor"
+    />
+  </svg>
+);
