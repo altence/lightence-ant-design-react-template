e1bb9874ccae2f743577edf4e9648d133fa03c01
armistrice <egor.leineveber@altence.co>
2021-10-12 12:07:13 +0300

    feat(profile-page-main-card): add tooltips for input suffixes, fix translation, add rest props for form

--- src/components/profile/ProfileCard/ProfileFormNav/nav/PersonalInfo/SocialLinksItem/InputItem/InputItem.tsx
+++ src/components/profile/ProfileCard/ProfileFormNav/nav/PersonalInfo/SocialLinksItem/InputItem/InputItem.tsx
@@ -1,9 +1,9 @@
-import React, { useCallback, useState } from 'react';
-import { Input } from 'components/common/inputs/Input/Input';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Button, Tooltip } from 'antd';
+import { useTranslation } from 'react-i18next';
 import { LinkOutlined } from '@ant-design/icons';
 import { FormItem } from 'components/profile/ProfileCard/ProfileFormNav/ProfileForm/ProfileForm.styles';
-import { Button, notification } from 'antd';
-import { useTranslation } from 'react-i18next';
+import { SuffixInput } from 'components/common/inputs/SuffixInput/SuffixInput';
 
 interface InputItemProps {
   name: string;
@@ -12,21 +12,30 @@ interface InputItemProps {
 
 export const InputItem: React.FC<InputItemProps> = ({ name, Addon }) => {
   const [socialLink, setSocialLink] = useState('');
+  const [isCopied, setCopied] = useState(false);
 
   const { t } = useTranslation();
 
   const handleCopy = useCallback(() => {
-    notification.open({ message: t('common.copy') });
+    setCopied(true);
 
     navigator.clipboard.writeText(socialLink);
+  }, [socialLink, setCopied]);
+
+  useEffect(() => {
+    setCopied(false);
   }, [socialLink]);
 
   return (
     <FormItem name={name}>
-      <Input
-        value={socialLink}
+      <SuffixInput
         addonBefore={<Addon />}
-        suffix={<Button type="text" size="small" icon={<LinkOutlined />} onClick={handleCopy} />}
+        isVisibleSuffix={!!socialLink}
+        suffix={
+          <Tooltip title={isCopied ? t('common.copied') : t('common.copy')}>
+            <Button type="text" size="small" icon={<LinkOutlined />} onClick={handleCopy} />
+          </Tooltip>
+        }
         onChange={(event) => setSocialLink(event.target.value)}
       />
     </FormItem>
