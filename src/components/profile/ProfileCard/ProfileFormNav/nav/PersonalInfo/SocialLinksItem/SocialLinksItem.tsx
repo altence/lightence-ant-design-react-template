72839c5c0911117f6ea4154c016862e8a99c31b0
armistrice <egor.leineveber@altence.co>
2021-10-13 17:15:46 +0300

    feat(app): add common clipboard and open url inputs, make icons bigger

--- src/components/profile/ProfileCard/ProfileFormNav/nav/PersonalInfo/SocialLinksItem/SocialLinksItem.tsx
+++ src/components/profile/ProfileCard/ProfileFormNav/nav/PersonalInfo/SocialLinksItem/SocialLinksItem.tsx
@@ -1,9 +1,9 @@
 import React, { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { InputItem } from './InputItem/InputItem';
 import { socialLinksData } from 'constants/socialLinksData';
 import { FormItem } from '../../../ProfileForm/ProfileForm.styles';
 import { Col, Row } from 'antd';
+import { ClipboardInput } from 'components/common/inputs/ClipboardInput/ClipboardInput';
 
 interface SocialLinksItemProps {
   socialLinks: {
@@ -20,11 +20,12 @@ export const SocialLinksItem: React.FC<SocialLinksItemProps> = ({ socialLinks })
     () =>
       socialLinksData.map((link) => (
         <Col key={link.id} xs={24} md={12}>
-          <InputItem
-            value={Object.entries(socialLinks).find((formLink) => formLink[0] === link.name)?.[1]}
-            name={link.name}
-            Addon={link.Icon}
-          />
+          <FormItem name={link.name}>
+            <ClipboardInput
+              value={Object.entries(socialLinks).find((formLink) => formLink[0] === link.name)?.[1]}
+              addonBefore={<link.Icon />}
+            />
+          </FormItem>
         </Col>
       )),
     [socialLinks],
