b6b4a4b6cea075b02bc779b371da54a5ea518180
armistrice <egor.leineveber@altence.co>
2021-10-15 13:31:15 +0300

    fix(payments-card): fix buttons align and cards width

--- src/components/profile/ProfileCard/ProfileFormNav/nav/Payments/PaymentMethod/PaymentCardCarousel/PaymentCardCarousel.styles.ts
+++ src/components/profile/ProfileCard/ProfileFormNav/nav/Payments/PaymentMethod/PaymentCardCarousel/PaymentCardCarousel.styles.ts
@@ -1,17 +1,81 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const SliderWrapper = styled.div`
+interface SliderWrapperProps {
+  length: number | undefined;
+}
+
+export const SliderWrapper = styled.div<SliderWrapperProps>`
   margin-bottom: 1.5rem;
+
+  & .rccs {
+    width: 100%;
+
+    & .rccs__card {
+      width: 100%;
+    }
+  }
+
+  @media only screen and ${(props) => props.theme.media.md} {
+    ${(props) =>
+      props.length &&
+      props.length === 1 &&
+      css`
+        & .swiper-slide {
+          display: flex;
+          justify-content: center;
+
+          & > div {
+            display: inline-block;
+            position: relative;
+          }
+        }
+      `}
+
+    & .rccs {
+      width: ${(props) => (props.length && props.length > 1 ? '100%' : '290px')};
+
+      & .rccs__card {
+        width: ${(props) => (props.length && props.length > 1 ? '100%' : '290px')};
+      }
+    }
+  }
 `;
 
 export const BtnWrapper = styled.div`
   display: flex;
   position: absolute;
-  top: calc(50% - 4rem);
-  left: calc(50% + 6rem);
-  transform: translate(-50%, -50%);
+  top: 0;
+  right: 0;
 
   & > button {
     color: ${(props) => props.theme.colors.text.secondary};
   }
 `;
+
+/* & .rccs {
+    ${(props) =>
+    props.length &&
+    props.length > 1 &&
+    css`
+      width: 100%;
+      height: 150px;
+    `};
+
+    @media only screen and ${(props) => props.theme.media.md} {
+      width: 290px;
+    }
+
+    & > .rccs__card {
+      ${(props) =>
+    props.length &&
+    props.length > 1 &&
+    css`
+      width: 100%;
+      height: 150px;
+    `};
+
+      @media only screen and ${(props) => props.theme.media.md} {
+        width: 290px;
+      }
+    }
+  } */
