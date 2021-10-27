b5d152d256f4b46859d50c9f14725ba64f097d36
armistrice <egor.leineveber@altence.co>
2021-10-27 13:45:23 +0300

    fix(sidebar): fix links

--- src/components/sider/SiderMenuLink/SiderMenuLink.styles.ts
+++ src/components/sider/SiderMenuLink/SiderMenuLink.styles.ts
@@ -3,5 +3,5 @@ import { Menu } from 'antd';
 import { MenuItemProps } from '../interfaces';
 
 export const MenuItem = styled(Menu.Item)<MenuItemProps>`
-  ${(props) => props.isActive && `background: ${props.theme.colors.main.primaryGradient}`}
+  ${(props) => props.isActive && `background: ${props.theme.colors.main.primaryGradient} !important`}
 `;
