9bb6d23ed5f6d55d203ca2a049e098a0738ba9fc
armistrice <egor.leineveber@altence.co>
2021-10-28 19:40:21 +0300

    fix(components): delete height expression

--- src/components/common/inputs/InputPassword/InputPassword.tsx
+++ src/components/common/inputs/InputPassword/InputPassword.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Input as AntInput, InputProps as AntInputProps } from 'antd';
-import { useResponsive } from 'hooks/useResponsive';
 
 interface InputPasswordProps extends AntInputProps {
   className?: string;
 }
 
-export const InputPassword: React.FC<InputPasswordProps> = ({ className, children, ...props }) => {
-  const { isTablet } = useResponsive();
-
-  return (
-    <AntInput.Password size={(isTablet && 'large') || 'middle'} className={className} {...props}>
-      {children}
-    </AntInput.Password>
-  );
-};
+export const InputPassword: React.FC<InputPasswordProps> = ({ className, children, ...props }) => (
+  <AntInput.Password size="large" className={className} {...props}>
+    {children}
+  </AntInput.Password>
+);
