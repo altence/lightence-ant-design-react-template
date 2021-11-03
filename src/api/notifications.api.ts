121794fe902c6308ac461207f67644a251d51e89
armistrice <egor.leineveber@altence.co>
2021-11-03 16:38:19 +0300

    feat(notifications): use api instead of constant

---
+++ src/api/notifications.api.ts
@@ -0,0 +1,45 @@
+import { RoutesEnum } from 'constants/navigation';
+
+export interface Message {
+  id: number;
+  description: string;
+}
+
+export interface Mention extends Message {
+  userName: string;
+  userIcon: string;
+  place: string;
+  href: string;
+}
+
+export type Notification = Mention | Message;
+
+export const getNotifications = (): Promise<Notification[]> => {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res([
+        {
+          id: 2,
+          description: 'header.notifications.loginAttempt',
+        },
+        {
+          id: 1,
+          description: 'header.notifications.successPayment',
+        },
+        {
+          id: 3,
+          description: 'header.notifications.serverError',
+        },
+        {
+          id: 4,
+          description: 'header.notifications.mention',
+          userName: 'Steve Manson',
+          userIcon:
+            'https://res.cloudinary.com/lapkinthegod/image/upload/v1629187274/young-male-doctor-white-uniform_x7dcrs.jpg',
+          place: 'dashboard.latestScreenings.title',
+          href: `${RoutesEnum.DASHBOARD_PAGE}#latest-screenings`,
+        },
+      ]);
+    }, 1000);
+  });
+};
