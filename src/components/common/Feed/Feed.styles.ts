a5820e78c9c12fa1a44f6a035fe7e1ff96d74483
Pavel Dubrouski <Joikervik@gmail.com>
2021-11-12 11:44:38 +0300

    feat(feed): add feed card

--- src/components/common/Feed/Feed.styles.ts
+++ src/components/common/Feed/Feed.styles.ts
@@ -3,11 +3,9 @@ import theme from '../../../styles/theme';
 
 export const NewsWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-
+  flex-direction: column;
   & > div {
-    margin-bottom: 1rem;
+    margin-bottom: 1.25rem;
 
     @media only screen and ${theme.media.xl} {
       margin-bottom: 1.5rem;
@@ -15,16 +13,6 @@ export const NewsWrapper = styled.div`
 
     @media only screen and ${theme.media.xxl} {
       margin-bottom: 2.5rem;
-    }
-  }
-
-  & > div:first-of-type {
-    @media only screen and ${theme.media.xl} {
-      margin-right: 0.875rem;
-    }
-
-    @media only screen and ${theme.media.xxl} {
-      margin-right: 1.25rem;
     }
   }
 `;
---
+++ src/components/common/FeedCard/FeedCard.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Dates } from '../../../constants/Dates';
+import { Avatar } from 'antd';
+import * as S from './FeedCard.styles';
+
+interface FeedCardProps {
+  author?: React.ReactNode;
+  keywords?: boolean;
+  imgUrl: string;
+  title: string;
+  date: number;
+  description: string;
+  avatar: string;
+}
+
+export const FeedCard: React.FC<FeedCardProps> = ({ imgUrl, title, date, description, author, avatar, keywords }) => {
+  const { t } = useTranslation();
+
+  return (
+    <S.Wrapper>
+      {keywords ? (
+        <S.Keywords placeholder={t('common.keywords')} />
+      ) : (
+        <S.BtnMore type="ghost">{t('common.moreDetails')}</S.BtnMore>
+      )}
+      <S.Header>
+        <Avatar src={avatar} size={43} />
+        <S.AuthorWrapper>
+          {author && <S.Author>{author}</S.Author>}
+          <S.DateTime>{Dates.format(date, 'L')}</S.DateTime>
+        </S.AuthorWrapper>
+      </S.Header>
+      <S.Image src={imgUrl} preview={false} />
+      <S.InfoWrapper>
+        <S.InfoHeader>
+          <S.Title>{title}</S.Title>
+        </S.InfoHeader>
+        <S.Description>{description}</S.Description>
+      </S.InfoWrapper>
+    </S.Wrapper>
+  );
+};
