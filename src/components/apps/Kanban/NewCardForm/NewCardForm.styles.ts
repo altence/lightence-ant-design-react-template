b0a460bdeebf0d201e7236928ab9cf604860fcf2
Pavel Dubrouski <Joikervik@gmail.com>
2021-11-12 14:05:03 +0300

    fix(kanban): rename AddForm to NewCardForm

--- src/components/apps/Kanban/NewCardForm/NewCardForm.styles.ts
+++ src/components/apps/Kanban/NewCardForm/NewCardForm.styles.ts
@@ -1,5 +1,4 @@
 import { Button } from 'antd';
-import { hexToRGB } from 'utils/utils';
 import styled from 'styled-components';
 import { FormItem } from 'components/common/Form/Form.styles';
 import { CardWrapper as Card } from 'react-trello/dist/styles/Base';
@@ -13,14 +12,11 @@ export const Btn = styled(Button)`
   width: 100%;
 `;
 
-export const TagsBtn = styled(Btn)`
-  color: ${(props) => hexToRGB(props.theme.colors.text.main, 0.3)};
-`;
-
 export const FormInput = styled(FormItem)`
   @media only screen and ${(props) => props.theme.media.xl} {
     margin-bottom: 1rem;
   }
+
   font-weight: ${(props) => props.theme.commonFontWeight.medium};
   max-height: 1.25rem;
   &:first-child {
