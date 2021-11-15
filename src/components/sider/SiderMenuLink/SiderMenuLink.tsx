be429b34363e14802dfb0b901c924f0489558118
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-11-15 13:46:54 +0300

    feat(sider): remove notification badges

--- src/components/sider/SiderMenuLink/SiderMenuLink.tsx
+++ src/components/sider/SiderMenuLink/SiderMenuLink.tsx
@@ -1,37 +1,16 @@
 import React from 'react';
-import { MenuItemProps, Row, Col } from 'antd';
+import { MenuItemProps } from 'antd';
 import { Link } from 'react-router-dom';
-import { Badge } from 'components/common/Badge/Badge';
-import { NotificationType } from 'components/common/Notification/Notification';
 import * as S from './SiderMenuLink.styles';
 
 interface SiderMenuLinkProps extends MenuItemProps {
   href: string;
   title: string;
-  notificationsCount?: number;
-  notificationsSeverity?: NotificationType;
   isActive: boolean;
 }
 
-export const SiderMenuLink: React.FC<SiderMenuLinkProps> = ({
-  icon,
-  href,
-  title,
-  notificationsCount,
-  notificationsSeverity,
-  isActive,
-  ...props
-}) => (
+export const SiderMenuLink: React.FC<SiderMenuLinkProps> = ({ icon, href, title, isActive, ...props }) => (
   <S.MenuItem icon={icon} {...props} isActive={isActive}>
-    <Link to={href}>
-      <Row gutter={[20, 20]} justify="space-between" align="middle">
-        <Col flex={1}>{title}</Col>
-        {notificationsCount && (
-          <Col>
-            <Badge severity={notificationsSeverity} count={notificationsCount} />
-          </Col>
-        )}
-      </Row>
-    </Link>
+    <Link to={href}>{title}</Link>
   </S.MenuItem>
 );
