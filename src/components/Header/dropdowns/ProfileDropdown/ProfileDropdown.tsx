ce67260d8363346649f934c96338697947e9c111
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-11-17 11:48:00 +0300

    feat(mobile header): update profile dropdown height

--- src/components/Header/dropdowns/ProfileDropdown/ProfileDropdown.tsx
+++ src/components/Header/dropdowns/ProfileDropdown/ProfileDropdown.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Col, Dropdown, Row } from 'antd';
+import { Avatar, Col, Dropdown, Row } from 'antd';
 import { useMediaQuery } from 'react-responsive';
 import { H6 } from 'components/common/typography/H6/H6';
 import { ProfileOverlay } from './ProfileOverlay/ProfileOverlay';
@@ -20,16 +20,13 @@ export const ProfileDropdown: React.FC = () => {
     <Dropdown overlay={<ProfileOverlay />} trigger={['click']}>
       <S.ProfileDropdownHeader as={Row} gutter={[10, 10]} align="middle">
         <Col>
-          <S.AvatarImg src={user.imgUrl} alt="User" shape="circle" />
+          <Avatar src={user.imgUrl} alt="User" shape="circle" size={36} />
         </Col>
         {isTablet && (
           <Col>
             <H6>{`${user.firstName} ${user.lastName[0]}`}</H6>
           </Col>
         )}
-        <Col>
-          <S.DownArrow />
-        </Col>
       </S.ProfileDropdownHeader>
     </Dropdown>
   ) : null;
