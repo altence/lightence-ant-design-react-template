ab9617eb2c8a3929ce8b7e1530bdff56786fbb39
Pavel Dubrouski <Joikervik@gmail.com>
2021-11-17 15:35:48 +0300

    feat(auth): update auth layout

--- src/components/layouts/auth/AuthLayout.styles.ts
+++ src/components/layouts/auth/AuthLayout.styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { Typography } from 'antd';
-import loginBackground from 'assets/images/login-bg.jpg';
+import loginBackground from 'assets/images/login-bg.webp';
 import theme from 'styles/theme';
 import { hexToRGB } from 'utils/utils';
 
@@ -17,10 +17,6 @@ export const BackgroundWrapper = styled.div`
   background: url(${loginBackground});
   background-size: cover;
   position: relative;
-
-  @media only screen and ${theme.media.xl} {
-    width: 60%;
-  }
 `;
 
 export const LoginWrapper = styled.div`
@@ -31,45 +27,6 @@ export const LoginWrapper = styled.div`
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
-`;
-
-export const Title = styled(Typography.Text)`
-  font-size: 2.25rem;
-  font-weight: 700;
-
-  color: ${(props) => props.theme.colors.text.secondary};
-
-  @media only screen and ${theme.media.md} {
-    font-size: 4rem;
-  }
-
-  @media only screen and ${theme.media.xl} {
-    font-size: 4.5rem;
-  }
-`;
-
-export const Subtitle = styled(Typography.Text)`
-  font-size: 1.5rem;
-  font-weight: 300;
-  margin-bottom: 4.5rem;
-
-  color: ${(props) => props.theme.colors.text.secondary};
-
-  @media only screen and ${theme.media.md} {
-    font-size: 2.25rem;
-  }
-
-  @media only screen and ${theme.media.xl} {
-    font-size: 2.5rem;
-  }
-`;
-
-export const FormWrapper = styled.div`
-  position: relative;
-  width: 40%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
 
 export const Copyright = styled(Typography.Text)`
--- src/components/layouts/auth/AuthLayout.tsx
+++ src/components/layouts/auth/AuthLayout.tsx
@@ -1,30 +1,14 @@
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
 import { Outlet } from 'react-router-dom';
-import theme from '../../../styles/theme';
 import * as S from './AuthLayout.styles';
 
 const AuthLayout: React.FC = () => {
-  const isDesktop = useMediaQuery({
-    query: theme.media.xl,
-  });
-
   return (
     <S.Wrapper>
       <S.BackgroundWrapper>
-        <S.LoginWrapper>
-          <S.Title>Altence</S.Title>
-          <S.Subtitle>Masakra</S.Subtitle>
-          {!isDesktop && <Outlet />}
-        </S.LoginWrapper>
-        {!isDesktop && <S.Copyright>2021 &copy; Altence. All Rights Reserved</S.Copyright>}
+        <S.LoginWrapper><Outlet /></S.LoginWrapper>
+        {<S.Copyright>2021 &copy; Altence. All Rights Reserved</S.Copyright>}
       </S.BackgroundWrapper>
-      {isDesktop && (
-        <S.FormWrapper>
-          <Outlet />
-          <S.Copyright>2021 &copy; Altence. All Rights Reserved</S.Copyright>
-        </S.FormWrapper>
-      )}
     </S.Wrapper>
   );
 };
