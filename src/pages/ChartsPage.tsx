3208b4bc0903cf3381a4bfa73002eed287d9b1fe
Pavel Dubrouski <Joikervik@gmail.com>
2021-11-26 17:24:07 +0300

    feat(charts): remove unused cahrts and update styles

--- src/pages/ChartsPage.tsx
+++ src/pages/ChartsPage.tsx
@@ -1,29 +1,30 @@
-import { Col, Row } from 'antd';
+import { Col } from 'antd';
 import { GradientStackedAreaChart } from 'components/charts/GradientStackedAreaChart/GradientStackedAreaChart';
 import { PieChart } from 'components/charts/PieChart/PieChart';
 import { BarAnimationDelayChart } from 'components/charts/BarAnimationDelayChart/BarAnimationDelayChart';
 import { ScatterChart } from 'components/charts/ScatterChart/ScatterChart';
 import { LineRaceChart } from 'components/charts/LineRaceChart/LineRaceChart';
+import * as S from './ChartsPage.styles';
 
 const ChartsPage: React.FC = () => {
   return (
-    <Row gutter={[30, 30]}>
-      <Col xs={24} md={16}>
+    <S.Row gutter={[30, 30]}>
+      <Col xs={24} md={15}>
         <GradientStackedAreaChart />
       </Col>
-      <Col xs={24} md={8}>
+      <Col xs={24} md={9}>
         <PieChart />
       </Col>
       <Col xs={24} md={24}>
         <LineRaceChart />
       </Col>
-      <Col xs={24} md={8}>
+      <Col xs={24} md={9}>
         <ScatterChart />
       </Col>
-      <Col xs={24} md={16}>
+      <Col xs={24} md={15}>
         <BarAnimationDelayChart />
       </Col>
-    </Row>
+    </S.Row>
   );
 };
 
