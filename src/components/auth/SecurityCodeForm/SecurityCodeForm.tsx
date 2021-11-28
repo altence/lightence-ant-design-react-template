171b361e5c181c166ef772be3361bad507f52a74
Pavel Dubrouski <Joikervik@gmail.com>
2021-11-28 12:56:56 +0300

    feat(auth): add loading to security form

--- src/components/auth/SecurityCodeForm/SecurityCodeForm.tsx
+++ src/components/auth/SecurityCodeForm/SecurityCodeForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Form, Image } from 'antd';
+import { useNavigate, Link } from 'react-router-dom';
+import { Form, Image, Spin } from 'antd';
 import { useTranslation } from 'react-i18next';
 import * as S from './SecurityCodeForm.styles';
 import * as Auth from 'components/layouts/auth/AuthLayout.styles';
@@ -8,16 +8,18 @@ import { VerificationCodeInput } from 'components/common/VerificationCodeInput/V
 import VerifyEmailImage from 'assets/images/verify-email.png';
 
 export const SecurityCodeForm: React.FC = () => {
-  const { t } = useTranslation();
   const [verifyCode, setVerifyCode] = useState('');
-
+  const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   useEffect(() => {
     if (verifyCode.length === 6) {
+      setIsLoading(true);
       setTimeout(() => {
+        setIsLoading(false);
         navigate('/auth/new-password');
-      }, 500);
+      }, 1000);
     }
   }, [verifyCode]);
 
@@ -34,8 +36,10 @@ export const SecurityCodeForm: React.FC = () => {
           </S.ImageWrapper>
           <Auth.FormTitle>{t('verifyEmail.title')}</Auth.FormTitle>
           <S.VerifyEmailDescription>{t('verifyEmail.description')}</S.VerifyEmailDescription>
-          <VerificationCodeInput autoFocus onChange={(value) => setVerifyCode(value)} />
-          <S.NoCodeText>{t('verifyEmail.noCode')}</S.NoCodeText>
+          {isLoading ? <Spin /> : <VerificationCodeInput autoFocus onChange={(value) => setVerifyCode(value)} />}
+          <Link to="/" target={'_blank'}>
+            <S.NoCodeText>{t('verifyEmail.noCode')}</S.NoCodeText>
+          </Link>
         </S.ContentWrapper>
       </Form>
     </Auth.FormWrapper>
