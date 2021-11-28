36dcb93ef83ad1fd1f9fd90be90c7f5d93a75ba4
Pavel Dubrouski <Joikervik@gmail.com>
2021-11-28 14:46:57 +0300

    fix(auth): fix input ref errors

--- src/components/common/inputs/Input/Input.tsx
+++ src/components/common/inputs/Input/Input.tsx
@@ -5,8 +5,8 @@ export interface InputProps extends AntInputProps {
   className?: string;
 }
 
-export const Input: React.FC<InputProps> = ({ className, children, ...props }) => (
-  <AntInput className={className} {...props}>
+export const Input = React.forwardRef<AntInput, InputProps>(({ className, children, ...props }, ref) => (
+  <AntInput ref={ref} className={className} {...props}>
     {children}
   </AntInput>
-);
+));
