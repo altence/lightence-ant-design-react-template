00f084f94d0e8a762016bdd933414682d487f5ed
armistrice <egor.leineveber@altence.com>
2021-11-29 13:29:19 +0300

    fix(components): update

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -44,7 +44,7 @@ export const hexToRGB = (hex: string, alpha: string | number): string => {
 export const getDifference = (value: number, prevValue: number): string | null =>
   prevValue !== 0 ? `${((Math.abs(value - prevValue) / prevValue) * 100).toFixed(0)}%` : '100%';
 
-export const getValueFromProp = (prop: string | number | [number, number]): string =>
+export const normalizeProp = (prop: string | number | [number, number]): string =>
   typeof prop === 'number' ? `${prop}px` : (Array.isArray(prop) && `${prop[0]}px ${prop[1]}px`) || prop.toString();
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
