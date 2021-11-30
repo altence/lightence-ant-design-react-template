9e9ca7e816d8e02df0af9b2c6ad8f56135fd9782
armistrice <egor.leineveber@altence.com>
2021-11-30 19:15:27 +0300

    fix(favourties-doctors): fix according to new design

--- src/components/dashboard/FavouritesDoctorsCard/DoctorCard/DoctorCard.tsx
+++ src/components/dashboard/FavouritesDoctorsCard/DoctorCard/DoctorCard.tsx
@@ -1,42 +1,78 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { specifities } from 'constants/specifities';
-import { Doctor } from 'api/doctors.api';
+import { Col, Image, Row } from 'antd';
+import { Dates } from 'constants/Dates';
 import * as S from './DoctorCard.styles';
 
-type DoctorCardProps = Pick<Doctor, 'name' | 'specifity' | 'rating' | 'imgUrl'>;
+interface DoctorCardProps {
+  name?: string;
+  speciality?: number;
+  rating?: number;
+  imgUrl?: string;
+  date: number;
+}
 
-export const DoctorCard: React.FC<DoctorCardProps> = ({ name, specifity, rating, imgUrl }) => {
+export const DoctorCard: React.FC<DoctorCardProps> = ({ name, speciality, rating, imgUrl, date }) => {
   const { t } = useTranslation();
 
-  const speciality = useMemo(() => {
-    let result;
-
-    specifities.forEach((spec) => {
-      if (spec.id === specifity) {
-        result = spec.name;
-      }
-    });
-
-    return result;
-  }, [specifities, specifity]);
+  const specifity = specifities.find((el) => el.id === speciality)?.name;
 
   return (
-    <S.Wrapper>
-      <S.Avatar shape="square" src={imgUrl} />
-      <S.RatingWrapper>
-        <S.Rating disabled defaultValue={rating} />
-        <S.Text>{rating}</S.Text>
-      </S.RatingWrapper>
-      <S.InfoWrapper>
-        <S.Title>{t('common.doctor')}</S.Title>
-        <S.Text>{name}</S.Text>
-        <S.Title>{t('common.specifity')}</S.Title>
-        <S.Text>{speciality}</S.Text>
-        <S.VisitWrapper>
-          <S.Title>{t('dashboard.favoriteDoctors.lastVisit')}</S.Title>
-        </S.VisitWrapper>
-      </S.InfoWrapper>
-    </S.Wrapper>
+    <S.DoctorRow gutter={[{}, { xxl: 10 }]}>
+      <Col span={24}>
+        <S.ImgWrapper>
+          <Image src={imgUrl} alt={name} preview={false} />
+        </S.ImgWrapper>
+      </Col>
+
+      <Col span={24}>
+        <Row align="middle" gutter={[10, 10]}>
+          <Col>
+            <S.Rating value={rating} disabled />
+          </Col>
+
+          <Col>
+            <S.Text>{rating}.0</S.Text>
+          </Col>
+        </Row>
+      </Col>
+
+      <Col span={24}>
+        <Row>
+          <Col span={24}>
+            <S.Title>{t('common.doctor')}</S.Title>
+          </Col>
+
+          <Col span={24}>
+            <S.Text>{name}</S.Text>
+          </Col>
+        </Row>
+      </Col>
+
+      <Col span={24}>
+        <Row>
+          <Col span={24}>
+            <S.Title>{t('common.specifity')}</S.Title>
+          </Col>
+
+          <Col span={24}>
+            <S.Text>{t(`common.${specifity}`)}</S.Text>
+          </Col>
+        </Row>
+      </Col>
+
+      <Col span={24}>
+        <Row justify="space-between" wrap={false}>
+          <Col>
+            <S.Title>{t('dashboard.favoriteDoctors.lastVisit')}</S.Title>
+          </Col>
+
+          <Col>
+            <S.Text>{Dates.format(date, 'L')}</S.Text>
+          </Col>
+        </Row>
+      </Col>
+    </S.DoctorRow>
   );
 };
