47021abe6ab66b55524c6e44c697723ed158e20e
Pavel Dubrouski <Joikervik@gmail.com>
2021-11-30 14:23:09 +0300

    feat(table): add scroll to table

--- src/components/tables/Tables/Tables.tsx
+++ src/components/tables/Tables/Tables.tsx
@@ -4,7 +4,6 @@ import { TreeTable } from '../TreeTable/TreeTable';
 import { EditableTable } from '../EditableTable/EditableTable';
 import { useTranslation } from 'react-i18next';
 import * as S from './Tables.styles';
-import { Card } from 'components/common/Card/Card';
 
 export const Tables: React.FC = () => {
   const { t } = useTranslation();
@@ -12,15 +11,15 @@ export const Tables: React.FC = () => {
     <>
       <S.Title>{t('tables.tables')}</S.Title>
       <S.TablesWrapper>
-        <Card title="Basic Table" padding="0 1.25rem">
+        <S.Card title="Basic Table" padding="0 1.25rem">
           <BasicTable />
-        </Card>
-        <Card title="Tree Table" padding="0 1.25rem">
+        </S.Card>
+        <S.Card title="Tree Table" padding="0 1.25rem">
           <TreeTable />
-        </Card>
-        <Card title="Editable Table" padding="0 1.25rem">
+        </S.Card>
+        <S.Card title="Editable Table" padding="0 1.25rem">
           <EditableTable />
-        </Card>
+        </S.Card>
       </S.TablesWrapper>
     </>
   );
