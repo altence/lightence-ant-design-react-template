2ddddf282e5e76ca210927010af199a60780b98e
armistrice <egor.leineveber@altence.com>
2021-12-01 15:53:21 +0300

    fix(covid): update

--- src/components/dashboard/CovidCard/CovidCard.tsx
+++ src/components/dashboard/CovidCard/CovidCard.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import { ChartData } from 'interfaces/interfaces';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Card } from '../../../components/common/Card/Card';
+import { DashboardCard } from '../DashboardCard/DashboardCard';
 import { CovidChart } from './CovidChart/CovidChart';
 
 export const CovidCard: React.FC = () => {
+  const [data] = useState<ChartData>([
+    820, 600, 700, 400, 600, 800, 700, 750, 500, 600, 700, 600, 800, 500, 700, 800, 850, 600, 400, 300, 450, 700, 400,
+    410, 420, 430, 480, 500, 360, 320,
+  ]);
+
   const { t } = useTranslation();
 
   return (
-    <Card id="covid" title={t('dashboard.covid')} padding={0}>
-      <CovidChart />
-    </Card>
+    <DashboardCard id="covid" title={t('dashboard.covid')} padding={0}>
+      <CovidChart data={data} />
+    </DashboardCard>
   );
 };
