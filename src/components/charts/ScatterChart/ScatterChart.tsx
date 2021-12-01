14e376f5ede0160587cdd2a4eef65700be94410a
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-12-01 12:57:11 +0300

    feat(charts): update Scatter chart data

--- src/components/charts/ScatterChart/ScatterChart.tsx
+++ src/components/charts/ScatterChart/ScatterChart.tsx
@@ -2,19 +2,34 @@ import React, { useContext } from 'react';
 import { Card } from 'components/common/Card/Card';
 import { Chart } from 'components/common/Chart/Chart';
 import { ThemeContext } from 'styled-components';
+import { useTranslation } from 'react-i18next';
 
 const data = [
-  [10.0, 8.04],
-  [8.0, 6.95],
-  [13.0, 7.58],
-  [9.0, 8.81],
-  [11.0, 8.33],
-  [14.0, 9.96],
-  [6.0, 7.24],
-  [4.0, 4.26],
-  [12.0, 10.84],
-  [7.0, 4.82],
-  [5.0, 5.68],
+  [170.0, 59.0],
+  [159.1, 47.6],
+  [166.0, 69.8],
+  [176.2, 66.8],
+  [160.2, 75.2],
+  [172.5, 55.2],
+  [170.9, 54.2],
+  [172.9, 62.5],
+  [153.4, 42.0],
+  [160.0, 50.0],
+  [147.2, 49.8],
+  [168.2, 49.2],
+  [175.0, 73.2],
+  [157.0, 47.8],
+  [167.6, 68.8],
+  [159.5, 50.6],
+  [175.0, 82.5],
+  [166.8, 57.2],
+  [176.5, 87.8],
+  [170.2, 72.8],
+  [174.0, 54.5],
+  [173.0, 59.8],
+  [179.9, 67.3],
+  [170.5, 67.8],
+  [160.0, 47.0],
 ];
 
 const markLineOpt = {
@@ -36,36 +51,76 @@ const markLineOpt = {
   ],
 };
 
-export const ScatterChart: React.FC = () => {
-  const theme = useContext(ThemeContext);
-  const option = {
-    color: theme.colors.main.primary,
-    grid: {
-      top: 10,
-      right: 10,
-      bottom: 0,
-      left: 5,
-      containLabel: true,
+const defaultOption = {
+  grid: {
+    top: 10,
+    right: 10,
+    bottom: 0,
+    left: 5,
+    containLabel: true,
+  },
+  tooltip: {
+    showDelay: 0,
+    // eslint-disable-next-line
+    formatter: function (params: any) {
+      if (params.value.length > 1) {
+        return params.value[0] + 'cm ' + params.value[1] + 'kg ';
+      } else {
+        return params.name + ' : ' + params.value + 'kg ';
+      }
     },
-    tooltip: {
-      formatter: 'Group {a}: ({c})',
-    },
-    xAxis: [{ gridIndex: 0, min: 0, max: 20 }],
-    yAxis: [{ gridIndex: 0, min: 0, max: 15 }],
-    series: [
-      {
-        type: 'scatter',
-        xAxisIndex: 0,
-        yAxisIndex: 0,
-        data: data,
-        markLine: markLineOpt,
+    axisPointer: {
+      show: true,
+      type: 'cross',
+      lineStyle: {
+        type: 'dashed',
+        width: 1,
       },
-    ],
-  };
+    },
+  },
+  xAxis: {
+    gridIndex: 0,
+    min: 140,
+    type: 'value',
+    scale: true,
+    axisLabel: {
+      formatter: '{value} kg',
+    },
+    splitLine: {
+      show: false,
+    },
+  },
+  yAxis: {
+    gridIndex: 0,
+    min: 40,
+    type: 'value',
+    scale: true,
+    axisLabel: {
+      formatter: '{value} cm',
+    },
+    splitLine: {
+      show: false,
+    },
+  },
+  series: [
+    {
+      type: 'scatter',
+      xAxisIndex: 0,
+      yAxisIndex: 0,
+      data: data,
+      markLine: markLineOpt,
+    },
+  ],
+};
+
+export const ScatterChart: React.FC = () => {
+  const { t } = useTranslation();
+  const theme = useContext(ThemeContext);
+  const option = { ...defaultOption, color: theme.colors.main.primary };
 
   return (
-    <Card padding="1.875rem 0.625rem 1.25rem 0.625rem">
-      <Chart option={option} height="20rem" />
+    <Card padding="0 0 1.875rem" title={t('charts.scatter')}>
+      <Chart option={option} />
     </Card>
   );
 };
