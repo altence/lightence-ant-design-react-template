d75cd95ef2c1b814f2d57d06b9dcfa4e30facc9f
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-12-02 23:02:12 +0300

    fix(dashboard): update PieceOfAdvice card styles

--- src/components/dashboard/PiecesOfAdviceCard/PiecesOfAdviceCard.styles.ts
+++ src/components/dashboard/PiecesOfAdviceCard/PiecesOfAdviceCard.styles.ts
@@ -1,35 +1,8 @@
 import styled from 'styled-components';
-import theme from '../../../styles/theme';
-import { AdviceCard } from '../../common/AdviceCard/AdviceCard';
-import { Card as CommonCard } from '../../common/Card/Card';
-
-export const Card = styled(CommonCard)``;
 
 export const Wrapper = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: space-around;
   flex-wrap: wrap;
-
-  @media only screen and ${theme.media.md} {
-    gap: 0.875rem;
-  }
-
-  @media only screen and ${theme.media.xl} {
-    gap: 1.25rem;
-  }
-`;
-
-export const Advice = styled(AdviceCard)`
-  && {
-    flex: 0.5;
-    min-width: 40%;
-
-    @media only screen and ${theme.media.xs} {
-      min-width: 100%;
-    }
-
-    @media only screen and ${theme.media.md} {
-      min-width: 40%;
-    }
-  }
+  gap: 1.25rem;
 `;
