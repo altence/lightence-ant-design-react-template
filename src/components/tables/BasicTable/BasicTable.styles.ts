1ff21b9d42d8537492196f5a527533a8a4578f91
Pavel Dubrouski <Joikervik@gmail.com>
2021-12-02 14:14:57 +0300

    fix(tables): fix naming and unused variables

--- src/components/tables/BasicTable/BasicTable.styles.ts
+++ src/components/tables/BasicTable/BasicTable.styles.ts
@@ -4,16 +4,10 @@ import { Tag as AntdTag } from 'antd';
 export const Tag = styled(AntdTag)`
   padding: 0.375rem 0;
   min-width: 7.5rem;
-  margin: 0;
+  margin: 0 0.75rem 0 0;
   text-align: center;
-  margin-right: 0.75rem;
 
   @media only screen and ${(props) => props.theme.media.md} {
     min-width: 6.5rem;
   }
-`;
-
-export const ActionsButtons = styled.div`
-  display: flex;
-  align-items: center;
 `;
