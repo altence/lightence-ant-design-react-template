a81b3458cafaff68a6b139f7864711fd7e7fe98b
Pavel Dubrouski <Joikervik@gmail.com>
2021-12-06 22:27:43 +0300

    feat(forms): add auth form

--- src/pages/AdvancedFormsPage.tsx
+++ src/pages/AdvancedFormsPage.tsx
@@ -3,6 +3,7 @@ import { Card as CommonCard } from 'components/common/Card/Card';
 import { DynamicForm } from 'components/forms/DynamicForm/DynamicForm';
 import { ControlForm } from 'components/forms/ControlForm/ControlForm';
 import { ValidationForm } from 'components/forms/ValidationForm/ValidationForm';
+import { AuthFrom } from 'components/forms/AuthForm/AuthForm';
 import { Col } from 'antd';
 import styled from 'styled-components';
 
@@ -22,6 +23,9 @@ const AdvancedFormsPage: React.FC = () => {
         </Card>
         <Card title="Validation form" padding="20px">
           <ValidationForm />
+        </Card>
+        <Card title="Auth form" padding="20px">
+          <AuthFrom />
         </Card>
       </Col>
     </>
