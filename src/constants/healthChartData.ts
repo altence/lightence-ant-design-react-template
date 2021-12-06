b35e07c7c73800deade12b1ff9905afc8a0d5b8c
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-12-06 19:57:44 +0300

    feat(health chart): update data

--- src/constants/healthChartData.ts
+++ src/constants/healthChartData.ts
@@ -6,23 +6,23 @@ export interface HealthFactor {
 
 export const healthChartData: HealthFactor[] = [
   {
-    value: 72,
+    value: 50,
     title: 'dashboard.health.lifestyle.title',
     description: 'dashboard.health.lifestyle.description',
   },
   {
-    value: 50,
+    value: 20,
     title: 'dashboard.health.ecology.title',
     description: 'dashboard.health.ecology.description',
   },
   {
-    value: 25,
+    value: 20,
     title: 'dashboard.health.genetics.title',
     description: 'dashboard.health.genetics.description',
   },
   {
-    value: 20,
-    title: 'dashboard.health.someInfo.title',
-    description: 'dashboard.health.someInfo.description',
+    value: 10,
+    title: 'dashboard.health.medicine.title',
+    description: 'dashboard.health.medicine.description',
   },
 ];
