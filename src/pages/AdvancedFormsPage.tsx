93f7fca02fbf41cf4de126be9962edaff92209b5
Pavel Dubrouski <Joikervik@gmail.com>
2021-12-06 22:45:37 +0300

    feat(forms): update forms layout

--- src/pages/AdvancedFormsPage.tsx
+++ src/pages/AdvancedFormsPage.tsx
@@ -4,29 +4,44 @@ import { DynamicForm } from 'components/forms/DynamicForm/DynamicForm';
 import { ControlForm } from 'components/forms/ControlForm/ControlForm';
 import { ValidationForm } from 'components/forms/ValidationForm/ValidationForm';
 import { AuthFrom } from 'components/forms/AuthForm/AuthForm';
-import { Col } from 'antd';
+import { Col, Row } from 'antd';
+import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 
 const Card = styled(CommonCard)`
-  margin-bottom: 20px;
+  margin-bottom: 1.25rem;
+  height: auto;
 `;
 
 const AdvancedFormsPage: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <>
-      <Col xl={12}>
-        <Card title="Dynamic form" padding="20px">
-          <DynamicForm />
-        </Card>
-        <Card title="Control form" padding="20px">
-          <ControlForm />
-        </Card>
-        <Card title="Validation form" padding="20px">
-          <ValidationForm />
-        </Card>
-        <Card title="Auth form" padding="20px">
-          <AuthFrom />
-        </Card>
+      <Col xl={24}>
+        <Row gutter={[20, 0]}>
+          <Col xs={24} md={12} xl={12}>
+            <Card title={t('forms.dynamicForm')} padding="1.25rem">
+              <DynamicForm />
+            </Card>
+          </Col>
+          <Col xs={24} md={12} xl={12}>
+            <Card title={t('forms.controlForm')} padding="1.25rem">
+              <ControlForm />
+            </Card>
+          </Col>
+        </Row>
+        <Row gutter={[20, 0]}>
+          <Col xs={24} md={18} xl={12}>
+            <Card title={t('forms.validationForm')} padding="1.25rem">
+              <ValidationForm />
+            </Card>
+          </Col>
+          <Col xs={24} md={18} xl={12}>
+            <Card title={t('forms.authForm')} padding="1.25rem">
+              <AuthFrom />
+            </Card>
+          </Col>
+        </Row>
       </Col>
     </>
   );
