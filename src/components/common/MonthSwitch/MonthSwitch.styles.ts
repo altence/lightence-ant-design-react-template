ba7961b203c92cf366ace7f88a1a86ad336057c1
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-12-14 16:58:18 +0300

    feat(month switch): update styles

--- src/components/common/MonthSwitch/MonthSwitch.styles.ts
+++ src/components/common/MonthSwitch/MonthSwitch.styles.ts
@@ -1,6 +1,14 @@
 import { Typography } from 'antd';
 import styled from 'styled-components';
 
+export const MonthSwitch = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 export const Text = styled(Typography.Text)`
   font-size: ${(props) => props.theme.commonFontSizes.xs};
+  margin: 0 1rem;
+  width: 7rem;
+  text-align: center;
 `;
