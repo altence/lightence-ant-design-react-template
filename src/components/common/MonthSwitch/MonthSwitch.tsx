ba76c329f157bd4d87f7cd3bed5ad3f828b9e272
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-12-14 16:58:18 +0300

    feat(month switch): update styles

--- src/components/common/MonthSwitch/MonthSwitch.tsx
+++ src/components/common/MonthSwitch/MonthSwitch.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Col, Row } from 'antd';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
-import { Card } from 'components/common/Card/Card';
 import * as S from './MonthSwitch.styles';
 import { AppDate, Dates } from 'constants/Dates';
 
@@ -13,20 +11,10 @@ interface MonthSwitchProps {
 
 export const MonthSwitch: React.FC<MonthSwitchProps> = ({ date, onIncrease, onDecrease }) => {
   return (
-    <Card padding={[12, 8]}>
-      <Row justify="center" gutter={[12, 12]}>
-        <Col>
-          <LeftOutlined onClick={onDecrease} />
-        </Col>
-
-        <Col>
-          <S.Text>{Dates.format(date, 'MMMM YYYY')}</S.Text>
-        </Col>
-
-        <Col>
-          <RightOutlined onClick={onIncrease} />
-        </Col>
-      </Row>
-    </Card>
+    <S.MonthSwitch>
+      <LeftOutlined onClick={onDecrease} />
+      <S.Text>{Dates.format(date, 'MMMM YYYY')}</S.Text>
+      <RightOutlined onClick={onIncrease} />
+    </S.MonthSwitch>
   );
 };
