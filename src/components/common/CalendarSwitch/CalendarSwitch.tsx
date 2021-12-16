2034ca45a7774b430fc595d2c1d6dc188570ff08
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-12-16 14:05:49 +0300

    feat(month switch): add i18n

--- src/components/common/CalendarSwitch/CalendarSwitch.tsx
+++ src/components/common/CalendarSwitch/CalendarSwitch.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import * as S from './CalendarSwitch.styles';
 import { Button } from '../buttons/Button/Button';
+import { useTranslation } from 'react-i18next';
 
 interface MonthSwitchProps {
   dateFormatted: string;
@@ -11,6 +12,8 @@ interface MonthSwitchProps {
 }
 
 export const CalendarSwitch: React.FC<MonthSwitchProps> = ({ dateFormatted, onIncrease, onDecrease, onToday }) => {
+  const { t } = useTranslation();
+
   return (
     <S.CalendarSwitch>
       <S.Text>{dateFormatted}</S.Text>
@@ -19,7 +22,7 @@ export const CalendarSwitch: React.FC<MonthSwitchProps> = ({ dateFormatted, onIn
           <LeftOutlined />
         </Button>
         <Button type="default" onClick={onToday} size="small">
-          TODAY
+          {t('common.today').toUpperCase()}
         </Button>
         <Button type="default" onClick={onIncrease} size="small">
           <RightOutlined />
