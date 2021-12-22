470bb10ab0984083a14bcc123ae341c03524201b
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-12-22 16:02:42 +0300

    feat(favourite doctors): update slider margins and rename the folder

---
+++ src/components/dashboard/favouriteDoctors/FavouritesDoctorsCard.tsx
@@ -0,0 +1,106 @@
+import React, { useContext, useEffect, useMemo, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Doctor, getDoctorsData } from 'api/doctors.api';
+import { DashboardCard } from '../DashboardCard/DashboardCard';
+import { Carousel } from 'components/common/Carousel/Carousel';
+import { DoctorCard } from './DoctorCard/DoctorCard';
+import styled, { ThemeContext } from 'styled-components';
+import { CalendarEvent, getUserCalendar } from 'api/calendar.api';
+import { getUser, User } from 'api/users.api';
+import { Dates } from 'constants/Dates';
+import { dashboardPaddings } from '../DashboardCard/DashboardCard';
+
+export const FavouritesDoctorsCard: React.FC = () => {
+  const { t } = useTranslation();
+  const [doctors, setDoctors] = useState<Doctor[]>([]);
+  const [user, setUser] = useState<User>();
+  const [calendar, setCalendar] = useState<CalendarEvent[]>([]);
+
+  const theme = useContext(ThemeContext);
+  const today = Dates.getToday();
+
+  useEffect(() => {
+    getDoctorsData().then((res) => setDoctors(res));
+  }, []);
+
+  useEffect(() => {
+    getUser().then((res) => setUser(res));
+  }, []);
+
+  useEffect(() => {
+    user && getUserCalendar(user.id).then((res) => setCalendar(res));
+  }, [user]);
+
+  const pastEvents = useMemo(
+    () => calendar.filter((event) => Dates.getDate(event.date).isBefore(today)),
+    [calendar, today],
+  );
+
+  return (
+    <DashboardCard title={t('dashboard.favoriteDoctors.title')}>
+      {doctors.length > 0 && calendar.length > 0 && (
+        <CarouselWrapper>
+          <Carousel
+            slidesToShow={6}
+            responsive={[
+              {
+                breakpoint: theme.breakpoints.xxl - 1,
+                settings: {
+                  slidesToShow: 5,
+                  slidesToScroll: 1,
+                },
+              },
+              {
+                breakpoint: theme.breakpoints.xl - 1,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 1,
+                },
+              },
+              {
+                breakpoint: theme.breakpoints.md - 1,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1,
+                },
+              },
+              {
+                breakpoint: theme.breakpoints.sm - 1,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                },
+              },
+            ]}
+          >
+            {pastEvents.map((event) => {
+              const currentDoctor = doctors.find((doctor) => doctor.id === event.doctor);
+
+              return (
+                <div key={currentDoctor?.id}>
+                  <DoctorCard
+                    imgUrl={currentDoctor?.imgUrl}
+                    name={currentDoctor?.name}
+                    speciality={currentDoctor?.specifity}
+                    rating={currentDoctor?.rating}
+                    date={event.date}
+                  />
+                </div>
+              );
+            })}
+          </Carousel>
+        </CarouselWrapper>
+      )}
+    </DashboardCard>
+  );
+};
+
+const CarouselWrapper = styled.div`
+  margin-left: -${dashboardPaddings.xs[1]}px;
+  margin-right: -${dashboardPaddings.xs[1]}px;
+
+  @media only screen and ${(props) => props.theme.media.md} {
+    margin-left: -${dashboardPaddings.md[1]}px;
+    margin-right: -${dashboardPaddings.md[1]}px;
+  }
+`;
