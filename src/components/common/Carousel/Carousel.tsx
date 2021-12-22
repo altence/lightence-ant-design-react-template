1c6ab029e6245100c046d9f7eb756cce8d94102b
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-12-22 15:36:39 +0300

    feat(carousel): update props

--- src/components/common/Carousel/Carousel.tsx
+++ src/components/common/Carousel/Carousel.tsx
@@ -1,40 +1,25 @@
 import React from 'react';
-import Slider, { ResponsiveObject } from 'react-slick';
+import Slider, { Settings } from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-interface CarouselProps {
-  className?: string;
-  slidesToShow?: number;
-  arrows?: boolean;
-  dots?: boolean;
-  infinite?: boolean;
-  nextArrow?: JSX.Element;
-  prevArrow?: JSX.Element;
-  responsive?: ResponsiveObject[];
-}
-
-export const Carousel: React.FC<CarouselProps> = ({
-  className,
+export const Carousel: React.FC<Settings> = ({
   slidesToShow = 1,
   arrows = false,
   dots = false,
-  infinite = false,
-  nextArrow,
-  prevArrow,
-  responsive,
+  infinite = true,
+  centerMode = true,
   children,
+  ...props
 }) => {
   return (
     <Slider
-      className={className}
       slidesToShow={slidesToShow}
       arrows={arrows}
       dots={dots}
       infinite={infinite}
-      nextArrow={nextArrow}
-      prevArrow={prevArrow}
-      responsive={responsive}
+      centerMode={centerMode}
+      {...props}
     >
       {children}
     </Slider>
