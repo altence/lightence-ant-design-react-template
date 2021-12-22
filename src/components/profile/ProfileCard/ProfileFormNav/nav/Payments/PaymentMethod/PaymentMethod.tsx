4147a6db0702576ec9cd276ebe66d9c27535484b
leineveber <egor.leineveber@altence.com>
2021-12-22 23:33:52 +0300

    refactor(payment-cards): separate in new constant

--- src/components/profile/ProfileCard/ProfileFormNav/nav/Payments/PaymentMethod/PaymentMethod.tsx
+++ src/components/profile/ProfileCard/ProfileFormNav/nav/Payments/PaymentMethod/PaymentMethod.tsx
@@ -8,6 +8,7 @@ import { CreditCard } from './PaymentForm/interfaces';
 import { cardThemes } from 'constants/cardThemes';
 import { PaymentModal } from './PaymentModal/PaymentModal';
 import { useResponsive } from 'hooks/useResponsive';
+import { cards as initialCards } from '@app/constants/data/cards';
 import * as S from './PaymentMethod.styles';
 
 export const clearCardData: CreditCard = {
@@ -24,44 +25,7 @@ export const PaymentMethod: React.FC = () => {
   const { t } = useTranslation();
   const [isModalVisible, setModalVisible] = useState(false);
   const [cardData, setCardData] = useState<CreditCard>(clearCardData);
-  const [cards, setCards] = useState<CreditCard[]>([
-    {
-      name: 'Dean James',
-      cvc: '144',
-      expiry: '11/24',
-      number: '4255 2003 0168 9006',
-      focused: '',
-      background: cardThemes[0].background,
-      isEdit: false,
-    },
-    {
-      name: 'Walker Alan',
-      cvc: '179',
-      expiry: '05/23',
-      number: '5245 0017 2000 0164',
-      focused: '',
-      background: cardThemes[3].background,
-      isEdit: false,
-    },
-    {
-      name: 'Jackson Michael',
-      cvc: '345',
-      expiry: '12/22',
-      number: '4255 1000 2046 8006',
-      focused: '',
-      background: cardThemes[5].background,
-      isEdit: false,
-    },
-    {
-      name: 'Ivanov Ivan',
-      cvc: '222',
-      expiry: '09/24',
-      number: '5205 0024 2535 5235',
-      focused: '',
-      background: cardThemes[4].background,
-      isEdit: false,
-    },
-  ]);
+  const [cards, setCards] = useState<CreditCard[]>(initialCards);
 
   const [form] = Form.useForm();
 
