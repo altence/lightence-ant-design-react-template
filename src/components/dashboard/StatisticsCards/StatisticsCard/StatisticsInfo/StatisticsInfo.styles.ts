2416db6ce877d7960051de940e8ad0fdb39d6e12
Eugeny.Sadovsky <eugeny.sadovsky@isglocal.isg>
2021-12-24 17:18:43 +0300

    feat(statisticCard): update text size

--- src/components/dashboard/StatisticsCards/StatisticsCard/StatisticsInfo/StatisticsInfo.styles.ts
+++ src/components/dashboard/StatisticsCards/StatisticsCard/StatisticsInfo/StatisticsInfo.styles.ts
@@ -2,8 +2,16 @@ import { Typography } from 'antd';
 import styled from 'styled-components';
 
 export const Title = styled(Typography.Text)`
-  font-size: ${(props) => props.theme.commonFontSizes.lg};
+  font-size: ${(props) => props.theme.commonFontSizes.xs};
   font-weight: ${(props) => props.theme.commonFontWeight.bold};
+
+  @media only screen and ${(props) => props.theme.media.md} {
+    font-size: ${(props) => props.theme.commonFontSizes.md};
+  }
+
+  @media only screen and ${(props) => props.theme.media.xl} {
+    font-size: ${(props) => props.theme.commonFontSizes.lg};
+  }
 `;
 
 export const Text = styled(Typography.Text)`
