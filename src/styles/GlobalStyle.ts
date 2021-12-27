c5a0634641ed0829cd7258e47ac323563406300d
Pavel Dubrouski <Joikervik@gmail.com>
2021-12-27 14:10:46 +0300

    feat(notifications): update notifications styles

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -39,4 +39,24 @@ export default createGlobalStyle`
   .d-none {
     display: none;
   }
+
+  .ant-notification-notice {
+    padding: 2.1875rem 3.125rem 2.5rem 3.125rem;
+    width: 35.3125rem
+  }
+
+  .ant-notification-notice-with-icon .ant-notification-notice-message {
+    margin-left: 4.375rem;
+    padding-top: 0.3125rem;
+    margin-bottom: 1.25rem;
+  }
+
+  .ant-notification-notice-with-icon .ant-notification-notice-description {
+    margin-left: 4.375rem;
+  }
+
+  .ant-notification-notice-icon {
+    font-size: 2.8125rem;
+    margin-left: 0
+  }
 `;
