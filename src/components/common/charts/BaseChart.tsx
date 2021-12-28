681173d7d7ed7e2219ee94ba9c722cbde4e895d8
leineveber <egor.leineveber@altence.com>
2021-12-28 17:13:52 +0300

    fix(base-chart): add func

--- src/components/common/charts/BaseChart.tsx
+++ src/components/common/charts/BaseChart.tsx
@@ -3,6 +3,7 @@ import { EChartsOption } from 'echarts-for-react';
 import ReactECharts from 'echarts-for-react';
 import { Loading } from '../Loading';
 import { getChartColors } from '@app/styles/theme';
+import { DefaultTheme } from 'styled-components';
 
 export interface BaseChartProps {
   option?: EChartsOption;
@@ -17,6 +18,28 @@ export interface BaseChartProps {
 const defaultOption = {
   color: getChartColors(),
 };
+
+interface DefaultTooltipStyles {
+  borderColor: string;
+  borderWidth: number;
+  borderRadius: number;
+  textStyle: {
+    fontWeight: number;
+    fontSize: number;
+    color: string;
+  };
+}
+
+export const getDefaultTooltipStyles = (theme: DefaultTheme): DefaultTooltipStyles => ({
+  borderColor: theme.colors.charts.color1,
+  borderWidth: 2,
+  borderRadius: Number.parseInt(theme.border.radius),
+  textStyle: {
+    fontWeight: 600,
+    fontSize: 16,
+    color: theme.colors.charts.color1,
+  },
+});
 
 export const BaseChart: React.FC<BaseChartProps> = ({ option, width, height, onEvents, style, ...props }) => {
   const [loading, setLoading] = useState(true);
