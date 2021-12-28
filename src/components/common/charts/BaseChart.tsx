3567b1bfaf312a305c0db49a3bf1e4649aea4adf
Alexander Rudnik <alexanderrud0603@gmail.com>
2021-12-28 14:12:59 +0300

    feat(base chart): add possibility to style chart

--- src/components/common/charts/BaseChart.tsx
+++ src/components/common/charts/BaseChart.tsx
@@ -1,22 +1,24 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect, useState } from 'react';
+import React, { CSSProperties, useEffect, useState } from 'react';
 import { EChartsOption } from 'echarts-for-react';
 import ReactECharts from 'echarts-for-react';
 import { Loading } from '../Loading';
-import { getChartColors } from '../../../styles/theme';
+import { getChartColors } from '@app/styles/theme';
 
 export interface BaseChartProps {
   option?: EChartsOption;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   onEvents?: Record<string, (e: any) => void>;
   width?: string | number;
   height?: string | number;
+  style?: CSSProperties;
+  classname?: string;
 }
 
 const defaultOption = {
   color: getChartColors(),
 };
 
-export const BaseChart: React.FC<BaseChartProps> = ({ option, width, height, onEvents }) => {
+export const BaseChart: React.FC<BaseChartProps> = ({ option, width, height, onEvents, style, ...props }) => {
   const [loading, setLoading] = useState(true);
 
   const chartHeight = height || '400px';
@@ -29,6 +31,11 @@ export const BaseChart: React.FC<BaseChartProps> = ({ option, width, height, onE
   return loading ? (
     <Loading />
   ) : (
-    <ReactECharts option={{ ...defaultOption, ...option }} style={{ height: chartHeight, width }} onEvents={onEvents} />
+    <ReactECharts
+      {...props}
+      option={{ ...defaultOption, ...option }}
+      style={{ ...style, height: chartHeight, width }}
+      onEvents={onEvents}
+    />
   );
 };
