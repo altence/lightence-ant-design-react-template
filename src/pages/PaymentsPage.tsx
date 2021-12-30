8b1c69ad48ae218e2d61ee55351fa46b4555161d
leineveber <egor.leineveber@altence.com>
2021-12-30 17:02:48 +0300

    feat(app): add temp pages for profile

---
+++ src/pages/PaymentsPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const PaymentsPage: React.FC = () => {
+  return <div></div>;
+};
---
+++ src/pages/SecuritySettingsPage.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const SecuritySettingsPage: React.FC = () => {
+  return <div></div>;
+};
