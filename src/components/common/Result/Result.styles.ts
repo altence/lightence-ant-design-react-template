bc619f19ac13eb83492d94ecfbf54e522bc6f875
Pavel Dubrouski <Joikervik@gmail.com>
2021-12-30 14:01:59 +0300

    fix(ui-components): update results colors

--- src/components/common/Result/Result.styles.ts
+++ src/components/common/Result/Result.styles.ts
@@ -1,4 +1,18 @@
 import styled from 'styled-components';
 import { Result as AntResult } from 'antd';
 
-export const Result = styled(AntResult)``;
+export const Result = styled(AntResult)`
+  &.ant-result-success .ant-result-icon > .anticon {
+    color: ${(props) => `${props.theme.colors.main.success}`};
+  }
+
+  &.ant-result-info .ant-result-icon > .anticon {
+    color: ${(props) => `${props.theme.colors.main.primary}`};
+  }
+  &.ant-result-warning .ant-result-icon > .anticon {
+    color: ${(props) => `${props.theme.colors.main.warning}`};
+  }
+  &.ant-result-error .ant-result-icon > .anticon {
+    color: ${(props) => `${props.theme.colors.main.error}`};
+  }
+`;
