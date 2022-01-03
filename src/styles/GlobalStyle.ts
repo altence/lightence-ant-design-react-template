df7132cd76f51d0b55fc6856c759bdd751858007
leineveber <egor.leineveber@altence.com>
2022-01-03 20:37:32 +0300

    feat(styles): add scroll styles to global styles

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -8,6 +8,19 @@ export default createGlobalStyle`
     padding: 0;
   }
 
+  ::-webkit-scrollbar {
+    width: 1rem;
+  }
+  ::-webkit-scrollbar-track {
+    background-color: transparent;
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.theme.commonColors.lightgrey};
+    border-radius: 1.25rem;
+    border: 0.375rem solid transparent;
+    background-clip: content-box;
+  }
+
   body {
     font-weight: 500;
   }
