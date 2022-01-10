31c2b854ac75a0e74724e2e3ade3ee36a3ac35f7
Alexander Rudnik <alexanderrud0603@gmail.com>
2022-01-10 17:24:28 +0300

    feat(profile payment): update `Add New Card` button styles

---
+++ src/components/profile/ProfileCard/ProfileFormNav/nav/Payments/PaymentMethod/AddNewCardButton/AddNewCardButton.styles.ts
@@ -0,0 +1,9 @@
+import { Button } from '@app/components/common/buttons/Button/Button';
+import styled from 'styled-components';
+
+export const AddBtn = styled(Button)`
+  width: 290px;
+  height: 182.86px;
+  font-size: 2rem;
+  border-radius: 14.5px;
+`;
