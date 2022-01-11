0b1bc513db5de5408661bcfd145bec0c92a7c2d9
Pavel Dubrouski <Joikervik@gmail.com>
2022-01-11 12:00:30 +0300

    fix(ui-components): fix breadcrumbs error

--- src/components/common/Breadcrumb/Breadcrumb.tsx
+++ src/components/common/Breadcrumb/Breadcrumb.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { BreadcrumbProps, BreadcrumbItemProps } from 'antd';
+import { BreadcrumbProps } from 'antd';
 import * as S from './Breadcrumb.styles';
 
-export const BreadcrumbItem: React.FC<BreadcrumbItemProps> = ({ children, ...props }) => {
-  return <S.BreadcrumbItem {...props}>{children}</S.BreadcrumbItem>;
-};
+export const BreadcrumbItem = S.BreadcrumbItem;
 
 export const Breadcrumb: React.FC<BreadcrumbProps> = ({ children, ...props }) => {
   return <S.Breadcrumb {...props}>{children}</S.Breadcrumb>;
