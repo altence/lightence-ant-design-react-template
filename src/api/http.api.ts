10b0e3f0c177160920ce4c652ae2adb55cb26ecf
Alexander Rudnik <alexanderrud0603@gmail.com>
2022-01-14 12:59:49 +0300

    feat(http): read token from local storage

--- src/api/http.api.ts
+++ src/api/http.api.ts
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import { AxiosError } from 'axios';
 import { ApiError } from '@app/api/ApiError';
+import { readToken } from '@app/services/localStorage.service';
 
 export const httpApi = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 httpApi.interceptors.request.use((config) => {
-  config.headers = { ...config.headers, Authorization: 'Bearer test' };
+  config.headers = { ...config.headers, Authorization: `Bearer ${readToken()}` };
 
   return config;
 });
