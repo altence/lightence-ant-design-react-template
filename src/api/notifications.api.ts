94b579f96f27bfe6cf1df839c6dab0ac9a2bf66a
Alexander Rudnik <alexanderrud0603@gmail.com>
2022-01-17 16:47:48 +0300

    feat(notifications): remove async stub request

--- src/api/notifications.api.ts
+++ src/api/notifications.api.ts
@@ -12,32 +12,26 @@ export interface Mention extends Message {
 
 export type Notification = Mention | Message;
 
-export const getNotifications = (): Promise<Notification[]> => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      res([
-        {
-          id: 2,
-          description: 'header.notifications.loginAttempt',
-        },
-        {
-          id: 1,
-          description: 'header.notifications.successPayment',
-        },
-        {
-          id: 3,
-          description: 'header.notifications.serverError',
-        },
-        {
-          id: 4,
-          description: 'header.notifications.mention',
-          userName: 'Steve Manson',
-          userIcon:
-            'https://res.cloudinary.com/lapkinthegod/image/upload/v1629187274/young-male-doctor-white-uniform_x7dcrs.jpg',
-          place: 'dashboard.latestScreenings.title',
-          href: `/#latest-screenings`,
-        },
-      ]);
-    }, 1000);
-  });
-};
+export const notifications = [
+  {
+    id: 2,
+    description: 'header.notifications.loginAttempt',
+  },
+  {
+    id: 1,
+    description: 'header.notifications.successPayment',
+  },
+  {
+    id: 3,
+    description: 'header.notifications.serverError',
+  },
+  {
+    id: 4,
+    description: 'header.notifications.mention',
+    userName: 'Steve Manson',
+    userIcon:
+      'https://res.cloudinary.com/lapkinthegod/image/upload/v1629187274/young-male-doctor-white-uniform_x7dcrs.jpg',
+    place: 'dashboard.latestScreenings.title',
+    href: `/#latest-screenings`,
+  },
+];
--- src/components/Header/dropdowns/NotificationsDropdown/NotificationsDropdown.tsx
+++ src/components/Header/dropdowns/NotificationsDropdown/NotificationsDropdown.tsx
@@ -1,17 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Dropdown } from 'antd';
 import { BellOutlined } from '@ant-design/icons';
 import { NotificationsOverlay } from './NotificationsOverlay/NotificationsOverlay';
 import { Badge } from 'components/common/Badge/Badge';
-import { getNotifications, Notification } from 'api/notifications.api';
+import { notifications as fetchedNotifications, Notification } from 'api/notifications.api';
 import { DropdownHeader } from '../../Header.styles';
 
 export const NotificationsDropdown: React.FC = () => {
-  const [notifications, setNotifications] = useState<Notification[]>([]);
-
-  useEffect(() => {
-    getNotifications().then((res) => setNotifications(res));
-  }, []);
+  const [notifications, setNotifications] = useState<Notification[]>(fetchedNotifications);
 
   return (
     <Dropdown
