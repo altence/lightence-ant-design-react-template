a711ac4eb7a9fa4f97e91393a5d95124c3a32683
leineveber <egor.leineveber@altence.com>
2022-01-18 00:31:02 +0300

    refactor(advanced-forms-page): use base card

--- src/pages/AdvancedFormsPage.tsx
+++ src/pages/AdvancedFormsPage.tsx
@@ -1,66 +1,40 @@
 import React from 'react';
-import { Card as CommonCard } from 'components/common/Card/Card';
+import { Card } from 'components/common/Card/Card';
 import { DynamicForm } from 'components/forms/DynamicForm/DynamicForm';
 import { ControlForm } from 'components/forms/ControlForm/ControlForm';
 import { ValidationForm } from 'components/forms/ValidationForm/ValidationForm';
 import { StepForm } from 'components/forms/StepForm/StepForm';
-import { Col } from 'antd';
+import { Col, Row } from 'antd';
 import { useTranslation } from 'react-i18next';
-import styled from 'styled-components';
-import lightTheme from '@app/styles/themes/light/lightTheme';
 
-const Card = styled(CommonCard)`
-  margin-bottom: 1.25rem;
-  height: auto;
-  align-items: center;
-
-  & .ant-card-head {
-    align-self: flex-start;
-  }
-
-  & .ant-card-body {
-    width: 100%;
-    max-width: 35rem;
-  }
-
-  @media only screen and ${lightTheme.media.xs} {
-    & .ant-card-body {
-      padding: 1.25rem 1rem;
-    }
-  }
-
-  @media only screen and ${lightTheme.media.md} {
-    & .ant-card-body {
-      padding: 1.25rem 0;
-    }
-  }
-`;
-const BigCard = styled(Card)`
-  & .ant-card-body {
-    max-width: 75rem;
-    align-items: center;
-  }
-`;
 const AdvancedFormsPage: React.FC = () => {
   const { t } = useTranslation();
   return (
-    <Col span={24}>
-      <Card id="dynamic-form" title={t('forms.dynamicForm')} padding="1.25rem">
-        <DynamicForm />
-      </Card>
+    <Row gutter={[30, 30]}>
+      <Col span={24}>
+        <Card id="dynamic-form" title={t('forms.dynamicForm')} padding="1.25rem">
+          <DynamicForm />
+        </Card>
+      </Col>
 
-      <Card id="control-form" title={t('forms.controlForm')} padding="1.25rem">
-        <ControlForm />
-      </Card>
+      <Col span={24}>
+        <Card id="control-form" title={t('forms.controlForm')} padding="1.25rem">
+          <ControlForm />
+        </Card>
+      </Col>
 
-      <BigCard id="validation form" title={t('forms.validationForm')} padding="1.25rem">
-        <ValidationForm />
-      </BigCard>
+      <Col span={24}>
+        <Card id="validation form" title={t('forms.validationForm')} padding="1.25rem">
+          <ValidationForm />
+        </Card>
+      </Col>
 
-      <BigCard id="step-form" title={t('forms.stepForm')} padding="1.25rem">
-        <StepForm />
-      </BigCard>
-    </Col>
+      <Col span={24}>
+        <Card id="step-form" title={t('forms.stepForm')} padding="1.25rem">
+          <StepForm />
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
