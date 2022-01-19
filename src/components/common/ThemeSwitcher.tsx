5aacfd319c638b9865d1b12d528dc5c14b38fcc7
Alexander Rudnik <alexanderrud0603@gmail.com>
2022-01-19 17:20:40 +0300

    feat(theme switcher): when all themes are loaded we hide init spinner

--- src/components/common/ThemeSwitcher.tsx
+++ src/components/common/ThemeSwitcher.tsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import styled, { css } from 'styled-components';
 import { useThemeSwitcher } from 'react-css-theme-switcher';
 
 interface ThemeSwitcherProps {
   theme: string;
 }
-
-const spinnerPath = `${process.env.PUBLIC_URL}/spinners/spinner.svg`;
 
 export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = ({ theme, children }) => {
   const { switcher, themes, status } = useThemeSwitcher();
@@ -15,6 +12,10 @@ export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = ({ theme, children })
   useEffect(() => {
     if (!isThemesLoaded && status === 'loaded') {
       setThemesLoaded(true);
+      const initSpinner = document.getElementById('initSpinner');
+      if (initSpinner) {
+        initSpinner.style.display = 'none';
+      }
     }
   }, [isThemesLoaded, setThemesLoaded, status]);
 
@@ -22,38 +23,5 @@ export const ThemeSwitcher: React.FC<ThemeSwitcherProps> = ({ theme, children })
     switcher({ theme: theme === 'dark' ? themes.dark : themes.light });
   }, [switcher, theme, themes.dark, themes.light]);
 
-  return (
-    <>
-      {children}
-      <ThemeSwitcherLoading $isShown={!isThemesLoaded && status !== 'loaded'}>
-        <img src={spinnerPath} alt="Styles loading..." />
-      </ThemeSwitcherLoading>
-    </>
-  );
+  return <>{children}</>;
 };
-
-interface ThemeSwitcherLoadingProps {
-  $isShown: boolean;
-}
-
-const ThemeSwitcherLoading = styled.div<ThemeSwitcherLoadingProps>`
-  position: fixed;
-  top: 0;
-  right: 0;
-  height: 100%;
-  width: 100%;
-  background-color: ${(props) => props.theme.colors.main.mainBackground};
-  z-index: -1;
-  transition: opacity 500ms ease;
-  opacity: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  ${(props) =>
-    props.$isShown &&
-    css`
-      opacity: 1;
-      z-index: 9999;
-    `}
-`;
