5aa8524da1ea5f0fb226c439f9d1c3be72fa5042
leineveber <egor.leineveber@altence.com>
2022-01-22 20:47:50 +0300

    fix(map): remount component on every change of theme

--- src/components/dashboard/MapCard/MapCard.tsx
+++ src/components/dashboard/MapCard/MapCard.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { DashboardCard } from '../DashboardCard/DashboardCard';
 import { DoctorsMap } from './DoctorsMap/DoctorsMap';
 import { Doctor, getDoctorsData } from 'api/doctors.api';
+import { useTheme } from 'styled-components';
 
 export const MapCard: React.FC = () => {
   const [doctors, setDoctors] = useState<Doctor[]>([]);
@@ -10,9 +11,14 @@ export const MapCard: React.FC = () => {
     getDoctorsData().then((res) => setDoctors(res));
   }, []);
 
+  const theme = useTheme();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const key = useMemo(() => Math.random(), [theme]); // create new key on every change of theme and remount map component
+
   return (
     <DashboardCard padding={0}>
-      <DoctorsMap doctors={doctors} />
+      <DoctorsMap key={key} doctors={doctors} />
     </DashboardCard>
   );
 };
