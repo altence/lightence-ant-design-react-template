fa27aa1ab6965254426530b4e21ebc63fa589e80
Alexander Rudnik <alexanderrud0603@gmail.com>
2022-01-25 17:14:25 +0300

    refactor(notifications): move notifications styles to the global styles in order to have access to the `theme` object

--- src/controllers/notificationController.tsx
+++ src/controllers/notificationController.tsx
@@ -1,105 +1,35 @@
 import React from 'react';
 import { notification } from 'antd';
 import styled from 'styled-components';
-import { CheckCircleFilled, ExclamationCircleFilled, InfoCircleFilled } from '@ant-design/icons';
-import { ReactComponent as ErrorIcon } from 'assets/icons/error-notification.svg';
-import lightTheme from '@app/styles/themes/light/lightTheme';
-
-interface NotificationTitleProps {
-  $color?: string;
-  $isOnlyTitle: boolean;
-}
+import { CheckCircleFilled, ExclamationCircleFilled, InfoCircleFilled, StopFilled } from '@ant-design/icons';
+import { ArgsProps } from 'antd/lib/notification';
 
 interface IconWrapperProps {
   $isOnlyTitle: boolean;
 }
 
 const IconWrapper = styled.div<IconWrapperProps>`
-  font-size: ${(props) => (props.$isOnlyTitle ? '2.1875rem' : '2.8125rem')};
-  line-height: 2.8125rem;
-  ${(props) =>
-    props.$isOnlyTitle &&
-    'display: flex; align-items: center; justify-content: center; height: 2.1875rem; width: 2.1875rem;'}
-`;
-
-const SuccessIcon = styled(CheckCircleFilled)`
-  color: ${lightTheme.colors.main.success};
-`;
-
-const InfoIcon = styled(InfoCircleFilled)`
-  color: ${lightTheme.colors.main.primary};
-`;
-
-const WarningIcon = styled(ExclamationCircleFilled)`
-  color: ${lightTheme.colors.main.warning};
-`;
-
-const NotificationTitle = styled.div<NotificationTitleProps>`
-  color: ${(props) => (props.$isOnlyTitle ? lightTheme.colors.text.main : props.color)};
-  font-size: ${(props) => (props.$isOnlyTitle ? lightTheme.commonFontSizes.md : lightTheme.commonFontSizes.xxl)};
-  font-weight: ${(props) =>
-    props.$isOnlyTitle ? lightTheme.commonFontWeight.semibold : lightTheme.commonFontWeight.bold};
-  height: ${(props) => (props.$isOnlyTitle ? '2.1875rem' : '2.8125rem')};
-  display: flex;
-  align-items: center;
-  margin-left: ${(props) => (props.$isOnlyTitle ? '0.625rem' : '1.5625rem')};
-`;
-
-const NotificationDescription = styled.div`
-  color: ${lightTheme.colors.text.main};
-  font-size: ${lightTheme.commonFontSizes.md};
-  font-weight: ${lightTheme.commonFontWeight.semibold};
-  line-height: 1.375rem;
+  font-size: ${(props) => (props.$isOnlyTitle ? '2rem' : '3rem')};
+  line-height: 2rem;
 `;
 
 const EmptyDescription = styled.div`
-  margin-top: -0.625rem;
+  margin-top: -0.75rem;
 `;
 
-type NotificationPlacement = 'topLeft' | 'topRight' | 'bottomLeft' | 'bottomRight';
-
-interface NotificationProps {
-  message: React.ReactNode;
-  description?: React.ReactNode;
-  btn?: React.ReactNode;
-  key?: string;
-  onClose?: () => void;
-  duration?: number | null;
-  icon?: React.ReactNode;
-  placement?: NotificationPlacement;
-  style?: React.CSSProperties;
-  prefixCls?: string;
-  className?: string;
-  onClick?: () => void;
-  top?: number;
-  bottom?: number;
-  getContainer?: () => HTMLElement;
-  closeIcon?: React.ReactNode;
-}
+type NotificationProps = ArgsProps;
 
 const openSuccessNotification = (config: NotificationProps): void => {
   notification.success({
     ...config,
+    duration: 0,
     icon: (
       <IconWrapper $isOnlyTitle={!config.description}>
-        <SuccessIcon />
+        <CheckCircleFilled className="success-icon" />
       </IconWrapper>
     ),
-    message: (
-      <NotificationTitle color={lightTheme.colors.main.success} $isOnlyTitle={!config.description}>
-        {config.message}
-      </NotificationTitle>
-    ),
-    description: config.description ? (
-      <NotificationDescription>{config.description}</NotificationDescription>
-    ) : (
-      <EmptyDescription />
-    ),
-    style: {
-      padding: config.description ? '1.9375rem 2.5rem' : '1.1875rem 2.5rem',
-      border: `1px solid ${lightTheme.colors.main.success}`,
-      background: lightTheme.colors.notifications.success,
-    },
+    message: <div className={`title ${!config.description && `title-only`}`}>{config.message}</div>,
+    description: config.description ? <div className="description">{config.description}</div> : <EmptyDescription />,
     className: config.description ? '' : 'notification-without-description',
   });
 };
@@ -109,24 +39,11 @@ const openInfoNotification = (config: NotificationProps): void => {
     ...config,
     icon: (
       <IconWrapper $isOnlyTitle={!config.description}>
-        <InfoIcon />
+        <InfoCircleFilled className="info-icon" />
       </IconWrapper>
     ),
-    message: (
-      <NotificationTitle color={lightTheme.colors.main.primary} $isOnlyTitle={!config.description}>
-        {config.message}
-      </NotificationTitle>
-    ),
-    description: config.description ? (
-      <NotificationDescription>{config.description}</NotificationDescription>
-    ) : (
-      <EmptyDescription />
-    ),
-    style: {
-      padding: config.description ? '1.9375rem 2.5rem' : '1.1875rem 2.5rem',
-      border: `1px solid ${lightTheme.colors.main.primary}`,
-      background: lightTheme.colors.notifications.primary,
-    },
+    message: <div className={`title ${!config.description && `title-only`}`}>{config.message}</div>,
+    description: config.description ? <div className="description">{config.description}</div> : <EmptyDescription />,
     className: config.description ? '' : 'notification-without-description',
   });
 };
@@ -136,24 +53,11 @@ const openWarningNotification = (config: NotificationProps): void => {
     ...config,
     icon: (
       <IconWrapper $isOnlyTitle={!config.description}>
-        <WarningIcon />
+        <ExclamationCircleFilled className="warning-icon" />
       </IconWrapper>
     ),
-    message: (
-      <NotificationTitle color={lightTheme.colors.main.warning} $isOnlyTitle={!config.description}>
-        {config.message}
-      </NotificationTitle>
-    ),
-    description: config.description ? (
-      <NotificationDescription>{config.description}</NotificationDescription>
-    ) : (
-      <EmptyDescription />
-    ),
-    style: {
-      padding: config.description ? '1.9375rem 2.5rem' : '1.1875rem 2.5rem',
-      border: `1px solid ${lightTheme.colors.main.warning}`,
-      background: lightTheme.colors.notifications.warning,
-    },
+    message: <div className={`title ${!config.description && `title-only`}`}>{config.message}</div>,
+    description: config.description ? <div className="description">{config.description}</div> : <EmptyDescription />,
     className: config.description ? '' : 'notification-without-description',
   });
 };
@@ -163,24 +67,11 @@ const openErrorNotification = (config: NotificationProps): void => {
     ...config,
     icon: (
       <IconWrapper $isOnlyTitle={!config.description}>
-        <ErrorIcon />
+        <StopFilled className="error-icon" />
       </IconWrapper>
     ),
-    message: (
-      <NotificationTitle color={lightTheme.colors.main.error} $isOnlyTitle={!config.description}>
-        {config.message}
-      </NotificationTitle>
-    ),
-    description: config.description ? (
-      <NotificationDescription>{config.description}</NotificationDescription>
-    ) : (
-      <EmptyDescription />
-    ),
-    style: {
-      padding: config.description ? '1.9375rem 2.5rem' : '1.1875rem 2.5rem',
-      border: `1px solid ${lightTheme.colors.main.error}`,
-      background: lightTheme.colors.notifications.error,
-    },
+    message: <div className={`title ${!config.description && `title-only`}`}>{config.message}</div>,
+    description: config.description ? <div className="description">{config.description}</div> : <EmptyDescription />,
     className: config.description ? '' : 'notification-without-description',
   });
 };
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -56,44 +56,6 @@ export default createGlobalStyle`
     display: none;
   }
 
-  .ant-notification-notice {
-    width: 35.3125rem
-  }
-
-  .ant-notification-notice-with-icon .ant-notification-notice-message {
-    margin-bottom: 0;
-    margin-left: 2.8125rem;
-  }
-
-  .ant-notification-notice-with-icon .ant-notification-notice-description {
-    margin-left: 4.375rem;
-    margin-top: 0;
-  }
-
-  .ant-notification-notice-icon {
-    font-size: 2.8125rem;
-    margin-left: 0
-  }
-
-  .ant-notification-notice-close {
-    top: 1.25rem;
-    right: 1.25rem;
-  }
-
-  .ant-notification-notice-close-x {
-    display: flex;
-    font-size: 0.9375rem;
-  }
-
-  .notification-without-description {
-    .ant-notification-notice-close {
-      top: 1.875rem;
-    }
-    .ant-notification-notice-with-icon .ant-notification-notice-description  {
-      margin-top: 0.625rem;
-    }
-  }
-
   .ant-picker-cell {
     color: ${(props) => props.theme.colors.text.superLight};
 
@@ -110,27 +72,126 @@ export default createGlobalStyle`
     color: ${(props) => props.theme.colors.text.light};
   }
 
-  .notification-success {
-    border: ${(props) => `1px solid ${props.theme.colors.main.success}`};
-    background: ${(props) => props.theme.colors.notifications.success};
-  }
+  // notifications start
+  .ant-notification-notice {
+    width: 36rem;
+    padding: 2rem;
+    min-height: 6rem;
+    
+    .ant-notification-notice-with-icon .ant-notification-notice-message {
+      margin-bottom: 0;
+      margin-left: 2.8125rem;
+    }
 
-  .notification-info {
-    border: ${(props) => `1px solid ${props.theme.colors.main.primary}`};
-    background: ${(props) => props.theme.colors.notifications.primary};
-  }
+    .ant-notification-notice-with-icon .ant-notification-notice-description {
+      margin-left: 4.375rem;
+      margin-top: 0;
+    }
 
-  .notification-warning {
-    border: ${(props) => `1px solid ${props.theme.colors.main.warning}`};
-    background: ${(props) => props.theme.colors.notifications.warning};
-  }
+    .ant-notification-notice-icon {
+      font-size: 2.8125rem;
+      margin-left: 0
+    }
 
-  .notification-success {
-    border: ${(props) => `1px solid ${props.theme.colors.main.error}`};
-    background: ${(props) => props.theme.colors.notifications.error};
+    .ant-notification-notice-close {
+      top: 1.25rem;
+      right: 1.25rem;
+    }
+
+    .ant-notification-notice-close-x {
+      display: flex;
+      font-size: 0.9375rem;
+    }
+
+    .notification-without-description {
+      .ant-notification-notice-close {
+        top: 1.875rem;
+      }
+      .ant-notification-notice-with-icon .ant-notification-notice-description  {
+        margin-top: 0.625rem;
+      }
+    }
+    
+    .title {
+      font-size: ${(props) => props.theme.commonFontSizes.xxl};
+      height: 3rem;
+      margin-left: 1.5rem;
+      display: flex;
+      align-items: center;
+      font-weight: ${(props) => props.theme.commonFontWeight.bold};
+
+      &.title-only {
+        color: ${(props) => props.theme.colors.text.main};
+        font-size: ${(props) => props.theme.commonFontSizes.md};
+        height: 2rem;
+        line-height: 2rem;
+        margin-left: 0.75rem;
+        font-weight: ${(props) => props.theme.commonFontWeight.semibold};
+      }
+  }
+  
+    .description {
+      color: #404040;
+      font-size: ${(props) => props.theme.commonFontSizes.md};
+      font-weight: ${(props) => props.theme.commonFontWeight.semibold};
+      line-height: 1.375rem;
+    }
+  
+    &.ant-notification-notice-success {
+      border: ${(props) => `1px solid ${props.theme.colors.main.success}`};
+      background: ${(props) => props.theme.colors.notifications.success};
+      
+      .title {
+        color: ${(props) => props.theme.colors.main.success};
+      }
+    }
+  
+    &.ant-notification-notice-info {
+      border: ${(props) => `1px solid ${props.theme.colors.main.primary}`};
+      background: ${(props) => props.theme.colors.notifications.primary};
+  
+      .title {
+        color: ${(props) => props.theme.colors.main.primary};
+      }
+    }
+  
+    &.ant-notification-notice-warning {
+      border: ${(props) => `1px solid ${props.theme.colors.main.warning}`};
+      background: ${(props) => props.theme.colors.notifications.warning};
+  
+      .title {
+        color: ${(props) => props.theme.colors.main.warning};
+      }
+    }
+  
+    &.ant-notification-notice-error {
+      border: ${(props) => `1px solid ${props.theme.colors.main.error}`};
+      background: ${(props) => props.theme.colors.notifications.error};
+  
+      .title {
+        color: ${(props) => props.theme.colors.main.error};
+      }
+    }
+  
+    .success-icon {
+      color: ${(props) => props.theme.colors.main.success};
+    }
+  
+    .info-icon {
+      color: ${(props) => props.theme.colors.main.primary};
+    }
+  
+    .warning-icon {
+      color: ${(props) => props.theme.colors.main.warning};
+    }
+  
+    .error-icon {
+      color: ${(props) => props.theme.colors.main.error};
+    }
   }
   
   .ant-menu-inline, .ant-menu-vertical {
     border-right: 0;
   }
+  // notifications end
 `;
