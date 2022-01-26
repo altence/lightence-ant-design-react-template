640a789007b055d662fa065fa337643f9cde3a9e
leineveber <egor.leineveber@altence.com>
2022-01-26 15:33:50 +0300

    fix(overlay): fix blur height

--- src/components/common/Overlay.tsx
+++ src/components/common/Overlay.tsx
@@ -6,14 +6,14 @@ interface OverlayProps {
 
 export default styled.div<OverlayProps>`
   position: absolute;
-  z-index: 2;
+  z-index: 1;
   height: 0;
 
   ${(props) =>
     props.show &&
     css`
       backdrop-filter: blur(6px);
-      width: 100%;
-      height: 100%;
+      width: 100vw;
+      height: 100vh;
     `}
 `;
