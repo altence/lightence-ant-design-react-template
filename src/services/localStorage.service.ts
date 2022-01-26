a2545892973a4fc1c9589b9ed130f2fd3c21818a
Alexander Rudnik <alexanderrud0603@gmail.com>
2022-01-26 11:42:27 +0300

    feat(login): skip auth for demo

--- src/services/localStorage.service.ts
+++ src/services/localStorage.service.ts
@@ -1,11 +1,41 @@
 import { UserModel } from '@app/domain/UserModel';
+import avatarImg from '@app/assets/avatars/avatar5.png';
+
+const testUser = {
+  id: 1,
+  firstName: 'Christopher',
+  lastName: 'Johnson',
+  imgUrl: avatarImg,
+  userName: '@john1989',
+  email: {
+    name: 'christopher.johnson@altence.com',
+    verified: true,
+  },
+  phone: {
+    number: '+18143519459',
+    verified: true,
+  },
+  sex: 'male',
+  birthday: '01/26/2022',
+  lang: 'en',
+  country: 'GB',
+  city: 'London',
+  address1: '14 London Road',
+  zipcode: 5211,
+  website: 'altence.com',
+  socials: {
+    twitter: '@altence_team',
+    facebook: 'https://facebook.com/groups/1076577369582221',
+    linkedin: 'https://linkedin.com/company/altence',
+  },
+};
 
 export const persistToken = (token: string): void => {
   localStorage.setItem('accessToken', token);
 };
 
 export const readToken = (): string => {
-  return localStorage.getItem('accessToken') || '';
+  return localStorage.getItem('accessToken') || 'bearerToken';
 };
 
 export const persistUser = (user: UserModel): void => {
@@ -15,7 +45,7 @@ export const persistUser = (user: UserModel): void => {
 export const readUser = (): UserModel | null => {
   const userStr = localStorage.getItem('user');
 
-  return userStr ? JSON.parse(userStr) : null;
+  return userStr ? JSON.parse(userStr) : testUser;
 };
 
 export const deleteToken = (): void => localStorage.removeItem('accessToken');
