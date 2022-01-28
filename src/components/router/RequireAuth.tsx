f32c4a485058bb4ddf41d5464af38abce75a70df
leineveber <egor.leineveber@altence.com>
2022-01-28 18:41:43 +0300

    refactor(require-auth): login if no token

--- src/components/router/RequireAuth.tsx
+++ src/components/router/RequireAuth.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { useAppSelector } from '@app/hooks/reduxHooks';
-import { Navigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Navigate, useNavigate } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '@app/hooks/reduxHooks';
+import { doLogin } from '@app/store/slices/authSlice';
+import { initValues } from '../auth/LoginForm/LoginForm';
 
 const RequireAuth: React.FC = ({ children }) => {
+  const dispatch = useAppDispatch();
   const token = useAppSelector((state) => state.auth.token);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) {
+      dispatch(doLogin(initValues))
+        .unwrap()
+        .then(() => navigate('/'));
+    }
+  }, [token, dispatch, navigate]);
 
   return token ? <>{children}</> : <Navigate to="/auth/login" replace />;
 };
