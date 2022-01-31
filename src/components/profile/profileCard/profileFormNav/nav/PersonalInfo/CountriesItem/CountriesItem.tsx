bd172f710a6088c4fd0e422c4172b546e9b4b88d
leineveber <egor.leineveber@altence.com>
2022-01-31 21:26:26 +0300

    fix(profile): add alt attr for imgs

--- src/components/profile/profileCard/profileFormNav/nav/PersonalInfo/CountriesItem/CountriesItem.tsx
+++ src/components/profile/profileCard/profileFormNav/nav/PersonalInfo/CountriesItem/CountriesItem.tsx
@@ -11,7 +11,7 @@ const countries = countryList.getData();
 const selectOptions = countries.map((country) => (
   <Option key={country.name} value={country.code}>
     <S.CountriesSpace align="center">
-      <ReactCountryFlag countryCode={country.code} svg />
+      <ReactCountryFlag countryCode={country.code} svg alt="country flag" />
       {country.name}
     </S.CountriesSpace>
   </Option>
