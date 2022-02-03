e098327cfe4db6ee54486cd478bee5d5863d348e
leineveber <egor.leineveber@altence.com>
2022-02-03 12:52:58 +0300

    feat(2fa): add availability to disable 2fa

--- src/components/profile/profileCard/profileFormNav/nav/SecuritySettings/twoFactorAuth/TwoFactorSwitch/TwoFactorSwitch.tsx
+++ src/components/profile/profileCard/profileFormNav/nav/SecuritySettings/twoFactorAuth/TwoFactorSwitch/TwoFactorSwitch.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Col, Row, Switch } from 'antd';
+import { Col, Popconfirm, Row, Switch } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { BaseButtonsForm } from '@app/components/common/forms/BaseButtonsForm/BaseButtonsForm';
+import { useDispatch } from 'react-redux';
+import { setUser } from '@app/store/slices/userSlice';
+import { useAppSelector } from '@app/hooks/reduxHooks';
 
 interface TwoFactorSwitchProps {
   isEnabled: boolean;
@@ -11,6 +14,24 @@ interface TwoFactorSwitchProps {
 export const TwoFactorSwitch: React.FC<TwoFactorSwitchProps> = ({ isEnabled, setEnabled }) => {
   const { t } = useTranslation();
 
+  const user = useAppSelector((state) => state.user.user);
+  const dispatch = useDispatch();
+
+  const handleSwitch = () => {
+    setEnabled(!isEnabled);
+
+    if (!isEnabled) {
+      const newUser = {
+        ...user,
+        twoFactorAuth: {
+          enabled: false,
+          type: null,
+        },
+      };
+      dispatch(setUser(newUser));
+    }
+  };
+
   return (
     <BaseButtonsForm.Item>
       <Row justify="space-between">
@@ -18,7 +39,16 @@ export const TwoFactorSwitch: React.FC<TwoFactorSwitchProps> = ({ isEnabled, set
           <BaseButtonsForm.Title>{t('profile.nav.securitySettings.twoFactorAuth')}</BaseButtonsForm.Title>
         </Col>
         <Col>
-          <Switch checked={isEnabled} onChange={setEnabled} />
+          <Popconfirm
+            title={
+              !isEnabled
+                ? t('profile.nav.securitySettings.enableTwoFactorAuth')
+                : t('profile.nav.securitySettings.disableTwoFactorAuth')
+            }
+            onConfirm={handleSwitch}
+          >
+            <Switch checked={isEnabled} />
+          </Popconfirm>
         </Col>
       </Row>
     </BaseButtonsForm.Item>
