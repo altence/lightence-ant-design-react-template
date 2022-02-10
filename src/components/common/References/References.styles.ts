43c21a5cb47734e801bfab70dfdbbd3e10e8341e
Eugeny.Sadovsky <eugen.sadovsky@altence.com>
2022-02-10 18:18:13 +0300

    feat(references): update direction for small screens

--- src/components/common/References/References.styles.ts
+++ src/components/common/References/References.styles.ts
@@ -4,13 +4,18 @@ import React from 'react';
 export const ReferencesWrapper = styled.div`
   width: 100%;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
+  align-items: start;
   flex-wrap: nowrap;
   margin-top: ${(props) => `${props.theme.mobileLayout.paddingVertical}`};
 
-  @media only screen and ${(props) => props.theme.media.md} {
-    margin-top: ${(props) => `${props.theme.desktopLayout.paddingVertical}`};
+  @media only screen and ${(props) => props.theme.media.sm} {
+    align-items: center;
+  }
+
+  @media only screen and ${(props) => props.theme.media.xl} {
+    flex-direction: row;
+    justify-content: space-between;
   }
 `;
 
@@ -19,6 +24,11 @@ export const Text = styled.span`
   align-items: center;
   white-space: pre-wrap;
   flex-wrap: wrap;
+  margin-bottom: 1rem;
+
+  @media only screen and ${(props) => props.theme.media.xl} {
+    margin-bottom: 0;
+  }
 `;
 
 export const Icons = styled.div`
