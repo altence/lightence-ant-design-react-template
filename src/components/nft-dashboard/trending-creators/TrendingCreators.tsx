2b9da2fa402ce938b4e99a6e811dd22e7263e372
Eugeny.Sadovsky <eugen.sadovsky@altence.com>
2022-02-20 17:11:39 +0300

    feat(nft-dashboard): add stories

--- src/components/nft-dashboard/trending-creators/TrendingCreators.tsx
+++ src/components/nft-dashboard/trending-creators/TrendingCreators.tsx
@@ -1,21 +1,42 @@
-import React, { useState } from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import React, { useRef, useState } from 'react';
 import * as S from './TrendingCreators.styles';
 import { TrendingCreatorsStory } from '@app/components/nft-dashboard/trending-creators/story/TrendingCreatorsStory';
-import { Story } from 'react-insta-stories/dist/interfaces';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export const TrendingCreators: React.FC = () => {
   const [isStoryOpened, setStoryOpened] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [stories, setStories] = useState<Story[]>([
+
+  const [authors] = useState([
     {
-      url: 'https://media.istockphoto.com/photos/african-american-student-attending-webinar-from-home-picture-id1300821205?s=612x612',
+      name: 1,
+      stories: [
+        {
+          url: 'https://media.istockphoto.com/photos/african-american-student-attending-webinar-from-home-picture-id1300821205?s=612x612',
+        },
+        {
+          url: 'https://media.istockphoto.com/photos/productivity-powered-by-digital-technology-picture-id1330965067?s=612x612',
+        },
+      ],
     },
     {
-      url: 'https://media.istockphoto.com/photos/productivity-powered-by-digital-technology-picture-id1330965067?s=612x612',
+      name: 2,
+      stories: [
+        {
+          url: 'https://media.istockphoto.com/photos/abstract-graphic-world-map-illustration-on-blue-background-big-data-picture-id1294021851?s=612x612',
+        },
+        {
+          url: 'https://media.istockphoto.com/photos/lots-of-multicoloured-cubes-moving-in-space-to-come-together-to-form-picture-id1312818032?s=612x612',
+        },
+      ],
     },
   ]);
 
-  // const stories: Story[] = ;
+  const sliderRef = useRef(null);
 
   return (
     <>
@@ -38,25 +59,34 @@ export const TrendingCreators: React.FC = () => {
               zIndex: 999,
             }}
           >
-            <S.StoriesStyled
-              stories={stories}
-              defaultInterval={1500}
-              width="100%"
-              height="100%"
-              currentIndex={currentIndex}
-              onAllStoriesEnd={() => {
-                setCurrentIndex(0);
-                setStories([
-                  {
-                    url: 'https://media.istockphoto.com/photos/abstract-graphic-world-map-illustration-on-blue-background-big-data-picture-id1294021851?s=612x612',
-                  },
-                  {
-                    url: 'https://media.istockphoto.com/photos/lots-of-multicoloured-cubes-moving-in-space-to-come-together-to-form-picture-id1312818032?s=612x612',
-                  },
-                ]);
-                // setStoryOpened(false);
-              }}
-            />
+            <Slider
+              slidesToShow={1}
+              slidesToScroll={1}
+              centerMode={false}
+              ref={sliderRef}
+              arrows={false}
+              dots={false}
+              lazyLoad="ondemand"
+              autoplay={true}
+              infinite={false}
+            >
+              {authors.map((author) => {
+                return (
+                  <S.StoriesStyled
+                    key={author.name}
+                    stories={author.stories}
+                    defaultInterval={1500}
+                    width="100%"
+                    height="100%"
+                    currentIndex={currentIndex}
+                    onAllStoriesEnd={() => {
+                      sliderRef.current.slickNext();
+                      setCurrentIndex(0);
+                    }}
+                  />
+                );
+              })}
+            </Slider>
           </div>
         ) : (
           <></>
