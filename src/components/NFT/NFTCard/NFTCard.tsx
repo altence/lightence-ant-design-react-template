0a3f42fb6c7367b2d0440c515b315f44495224c5
leineveber <egor.leineveber@altence.com>
2022-02-22 21:15:56 +0300

    refactor(nft-cards): use common nft card

---
+++ src/components/NFT/NFTCard/NFTCard.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Card } from '@app/components/common/Card/Card';
+
+export const NFTCard: typeof Card = ({ ...props }) => {
+  return <Card autoHeight={false} padding={20} {...props} />;
+};
