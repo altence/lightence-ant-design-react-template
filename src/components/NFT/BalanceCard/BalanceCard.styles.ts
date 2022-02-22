a12ab5c6d0c34472dad9c371315b02cad597b64f
leineveber <egor.leineveber@altence.com>
2022-02-22 19:22:04 +0300

    feat(nft-dashboard): add balance card

---
+++ src/components/NFT/BalanceCard/BalanceCard.styles.ts
@@ -0,0 +1,10 @@
+import { Typography } from 'antd';
+import styled from 'styled-components';
+
+export const TitleBalanceText = styled(Typography.Title)`
+  &.ant-typography {
+    margin-bottom: 0;
+  }
+`;
+
+export const SubtitleBalanceText = styled(Typography.Text)``;
