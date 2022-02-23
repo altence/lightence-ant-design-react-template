bb4153cafaf0251e216f1aa3dd32198954336aec
Eugeny.Sadovsky <eugen.sadovsky@altence.com>
2022-02-23 15:09:07 +0300

    feat(nft-dashboard): recently added nfts

--- src/components/nft-dashboard/recently-added/RecentlyAddedNft.tsx
+++ src/components/nft-dashboard/recently-added/RecentlyAddedNft.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from './RecentlyAddedNft.styles';
 import { NftCard } from '@app/components/nft-dashboard/recently-added/nft-card/NftCard';
+import { getRecentlyAddedNfts, NftItem } from '@app/api/nftDashboard.api';
 
 export const RecentlyAddedNft: React.FC = () => {
+  const [nfts, setNfts] = useState<NftItem[]>([]);
+
+  useEffect(() => {
+    getRecentlyAddedNfts().then((result) => {
+      setNfts(result);
+    });
+  }, []);
+
   return (
     <>
       <S.SectionHeader>
         <S.h1>Recently Added NFTs</S.h1>
       </S.SectionHeader>
       <S.SectionContent>
-        <NftCard />
+        {nfts.length > 0 && nfts.map((nft) => <NftCard key={nft.title} nftItem={nft} />)}
         <S.ViewAll>
           <S.ViewAllLink>View All</S.ViewAllLink>
         </S.ViewAll>
