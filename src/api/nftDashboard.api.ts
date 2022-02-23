dc344c734847f02e77c0a778bb15ac5a497b08a6
Eugeny.Sadovsky <eugen.sadovsky@altence.com>
2022-02-23 18:04:19 +0300

    feat(nft-dashboard): style recently added section

--- src/api/nftDashboard.api.ts
+++ src/api/nftDashboard.api.ts
@@ -1,4 +1,6 @@
-import nft from '@app/assets/images/nft/nft/nft-image.png';
+import birthOfUniverse from '@app/assets/images/nft/nft/borth-of-universe.png';
+import starAway from '@app/assets/images/nft/nft/star-away.png';
+import nightDeprivation from '@app/assets/images/nft/nft/night-deprivation.png';
 
 export interface NftItem {
   image: string;
@@ -12,11 +14,25 @@ export const getRecentlyAddedNfts = (): Promise<NftItem[]> => {
   return new Promise((res) => {
     res([
       {
-        image: nft,
-        title: 'Birth of Universe',
+        image: birthOfUniverse,
+        title: 'Borth of Universe',
         author: 'jakeparker',
         currentBid: '3,150',
         currentBidCrypto: '1,02',
+      },
+      {
+        image: starAway,
+        title: 'Star Away',
+        author: 'nick_johnson',
+        currentBid: '7,210',
+        currentBidCrypto: '2,08',
+      },
+      {
+        image: nightDeprivation,
+        title: 'Night Deprivation',
+        author: 'mikeAdamson',
+        currentBid: '740',
+        currentBidCrypto: '1,56',
       },
     ]);
   });
