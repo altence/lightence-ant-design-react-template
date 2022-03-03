7f24c705d31089e585be2f9e1df094e1e90c0e49
leineveber <egor.leineveber@altence.com>
2022-03-03 23:42:54 +0300

    refactor(activity-story): overflow without title

--- src/components/nft-dashboard/activityStory/ActivityStory.styles.ts
+++ src/components/nft-dashboard/activityStory/ActivityStory.styles.ts
@@ -1,6 +1,17 @@
-import { Typography } from 'antd';
+import { Row, Typography } from 'antd';
 import styled from 'styled-components';
 
 export const Title = styled(Typography.Text)`
   font-weight: ${(props) => props.theme.commonFontWeight.semibold};
+`;
+
+export const ActivityRow = styled(Row)`
+  overflow-y: auto;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  height: 100%;
 `;
