0c8909096661169a9cebc60a5510e6dec0e6de09
leineveber <egor.leineveber@altence.com>
2022-03-10 19:46:06 +0300

    feat(carousel): add scroll func

--- src/components/common/Carousel/Carousel.tsx
+++ src/components/common/Carousel/Carousel.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+import React, { useCallback, useEffect, useRef } from 'react';
 import Slider, { Settings } from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -6,9 +8,54 @@ import 'slick-carousel/slick/slick-theme.css';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const Carousel = React.forwardRef<any, Settings>(
   ({ slidesToShow = 1, arrows = false, dots = false, infinite = true, centerMode = true, children, ...props }, ref) => {
+    const carouselRef = useRef();
+
+    const totalRef = ref?.current ? ref : carouselRef;
+
+    const handleScroll = useCallback(
+      (event) => {
+        const x = event.deltaX;
+
+        if (x > 0) {
+          return totalRef?.current?.slickNext();
+        } else {
+          return totalRef?.current?.slickPrev();
+        }
+      },
+      [totalRef],
+    );
+
+    const handleMouseOn = useCallback(() => {
+      document.body.style.overscrollBehaviorX = 'none';
+    }, []);
+
+    const handleMouseOff = useCallback(() => {
+      document.body.style.overscrollBehaviorX = 'unset';
+    }, []);
+
+    useEffect(() => {
+      if (totalRef.current) {
+        const slickList = totalRef.current?.innerSlider?.list;
+
+        slickList.addEventListener('wheel', handleScroll);
+
+        slickList.addEventListener('mouseover', handleMouseOn);
+
+        slickList.addEventListener('mouseout', handleMouseOff);
+
+        return () => {
+          slickList.removeEventListener('wheel', handleScroll);
+
+          slickList.removeEventListener('mouseover', handleMouseOn);
+
+          slickList.removeEventListener('mouseout', handleMouseOff);
+        };
+      }
+    }, [totalRef, handleScroll, handleMouseOn, handleMouseOff]);
+
     return (
       <Slider
-        ref={ref}
+        ref={totalRef}
         slidesToShow={slidesToShow}
         arrows={arrows}
         dots={dots}
