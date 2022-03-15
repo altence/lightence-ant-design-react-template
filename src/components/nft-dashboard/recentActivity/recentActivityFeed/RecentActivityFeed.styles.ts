792c82cad60f4d0c5bef43fcc3b08acb9dcec6f3
leineveber <egor.leineveber@altence.com>
2022-03-15 23:44:21 +0300

    fix(recent-activity-feed): fix filter bug

--- src/components/nft-dashboard/recentActivity/recentActivityFeed/RecentActivityFeed.styles.ts
+++ src/components/nft-dashboard/recentActivity/recentActivityFeed/RecentActivityFeed.styles.ts
@@ -1,11 +1,7 @@
 import styled from 'styled-components';
 
-interface FeedWrapperInternalProps {
-  $length: number;
-}
-
-export const FeedWrapper = styled.div<FeedWrapperInternalProps>`
+export const FeedWrapper = styled.div`
   overflow-y: auto;
 
-  max-height: ${(props) => (props.$length > 2 ? '500px' : '250px')}; // TODO find out a better solution
+  max-height: 500px;
 `;
