cf9497cd404b92b20fb0e36ac45c8748be121e63
leineveber <egor.leineveber@altence.com>
2022-03-15 20:36:41 +0300

    refactor(balance): refactor according to the design

--- src/components/nft-dashboard/Balance/Balance.styles.ts
+++ src/components/nft-dashboard/Balance/Balance.styles.ts
@@ -1,10 +1,24 @@
 import { Typography } from 'antd';
 import styled from 'styled-components';
 
-export const TitleBalanceText = styled(Typography.Title)`
+export const TitleText = styled(Typography.Title)`
   &.ant-typography {
-    margin-bottom: 0;
+    margin-bottom: 1.25rem;
+
+    font-size: ${(props) => props.theme.commonFontSizes.xs};
   }
 `;
 
-export const SubtitleBalanceText = styled(Typography.Text)``;
+export const TitleBalanceText = styled(Typography.Title)`
+  &.ant-typography {
+    margin-bottom: 0;
+
+    font-family: ${(props) => props.theme.fonts.secondaryFont};
+  }
+`;
+
+export const SubtitleBalanceText = styled(Typography.Text)`
+  font-family: ${(props) => props.theme.fonts.secondaryFont};
+
+  font-size: ${(props) => props.theme.commonFontSizes.xs};
+`;
