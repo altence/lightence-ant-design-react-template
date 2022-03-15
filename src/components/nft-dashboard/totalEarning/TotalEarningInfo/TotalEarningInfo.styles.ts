abac3e895fa05e8267a1f7683b52fe47f5241b78
leineveber <egor.leineveber@altence.com>
2022-03-15 20:49:14 +0300

    refactor(total-earning): refactor according to the design

--- src/components/nft-dashboard/totalEarning/TotalEarningInfo/TotalEarningInfo.styles.ts
+++ src/components/nft-dashboard/totalEarning/TotalEarningInfo/TotalEarningInfo.styles.ts
@@ -4,5 +4,15 @@ import styled from 'styled-components';
 export const Title = styled(Typography.Title)`
   &.ant-typography {
     margin-bottom: 0;
+
+    font-size: ${(props) => props.theme.commonFontSizes.xs};
   }
+`;
+
+export const Text = styled(Typography.Text)`
+  font-size: ${(props) => props.theme.commonFontSizes.xs};
+
+  font-weight: ${(props) => props.theme.commonFontWeight.regular};
+
+  font-family: ${(props) => props.theme.fonts.secondaryFont};
 `;
--- src/components/nft-dashboard/totalEarning/TotalEarningInfo/TotalEarningInfo.tsx
+++ src/components/nft-dashboard/totalEarning/TotalEarningInfo/TotalEarningInfo.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { Col, Row, Typography } from 'antd';
+import { Col, Row } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { formatNumberWithCommas, getCurrencyPrice } from '@app/utils/utils';
 import * as S from './TotalEarningInfo.styles';
-
-const { Text } = Typography;
 
 interface TotalEarningInfoProps {
   value: number;
@@ -20,7 +18,7 @@ export const TotalEarningInfo: React.FC<TotalEarningInfoProps> = ({ value }) => 
       </Col>
 
       <Col span={24}>
-        <Text>{getCurrencyPrice(formatNumberWithCommas(value), 'USD')}</Text>
+        <S.Text>{getCurrencyPrice(formatNumberWithCommas(value), 'USD')}</S.Text>
       </Col>
     </Row>
   );
