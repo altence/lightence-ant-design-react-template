c057187927fc805b17a525fe13563eb0a5846a1a
leineveber <egor.leineveber@altence.com>
2022-03-29 17:59:43 +0300

    refactor(slider): set theme on load

--- src/hooks/useAutoNightMode.ts
+++ src/hooks/useAutoNightMode.ts
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { setTheme } from '@app/store/slices/themeSlice';
 import { useAppDispatch, useAppSelector } from '@app/hooks/reduxHooks';
 import { Dates } from '@app/constants/Dates';
+import { msToH } from '@app/utils/utils';
 
 const getNextTime = (ms: number) => {
   const now = Date.now();
@@ -13,26 +14,49 @@ const getNextTime = (ms: number) => {
   return delta < 0 ? delta + 24 * 3600 * 1000 : delta;
 };
 
+const isNight = (nightTime: number[]) => {
+  const clearDate = Dates.getClearDate();
+
+  let now = Dates.getToday();
+
+  if (nightTime[0] < nightTime[1]) {
+    return now.isBetween(clearDate.add(nightTime[0], 'ms'), clearDate.add(nightTime[1], 'ms'), null, '[)');
+  } else {
+    now = now.hour() < msToH(nightTime[0]) ? now.add(24 * 3600 * 1000, 'ms') : now;
+    return now.isBetween(
+      clearDate.add(nightTime[0], 'ms'),
+      clearDate.add(nightTime[1] + 24 * 3600 * 1000, 'ms'),
+      null,
+      '[)',
+    );
+  }
+};
+
 let timeoutNightStarts: null | ReturnType<typeof setTimeout> = null;
 let timeoutNightEnds: null | ReturnType<typeof setTimeout> = null;
 
 export const useAutoNightMode = (): void => {
   const dispatch = useAppDispatch();
-  const nightModeState = useAppSelector((state) => state.nightMode);
-  const isNightMode = nightModeState.isNightMode;
-  const nightTime = nightModeState.nightTime;
+  const { isNightMode, nightTime } = useAppSelector((state) => state.nightMode);
 
   useEffect(() => {
     timeoutNightStarts && clearTimeout(timeoutNightStarts);
     timeoutNightEnds && clearTimeout(timeoutNightEnds);
 
     if (isNightMode) {
+      if (isNight(nightTime)) {
+        dispatch(setTheme('dark'));
+      } else {
+        dispatch(setTheme('light'));
+      }
+
       const runTimeoutStart = () => {
         dispatch(setTheme('dark'));
         timeoutNightStarts = setTimeout(runTimeoutStart, 24 * 3600 * 1000);
       };
 
       const nextStartTime = getNextTime(nightTime[0]);
+
       timeoutNightStarts = setTimeout(runTimeoutStart, nextStartTime);
 
       const runTimeoutEnd = () => {
