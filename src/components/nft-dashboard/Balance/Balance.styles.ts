aadeee41a0c95fc23b13d2e9be77ec929b075221
leineveber <egor.leineveber@altence.com>
2022-04-11 22:29:09 +0300

    refactor(balance): change button type on dark theme and color, make more distance between text

--- src/components/nft-dashboard/Balance/Balance.styles.ts
+++ src/components/nft-dashboard/Balance/Balance.styles.ts
@@ -1,3 +1,4 @@
+import { Button } from '@app/components/common/buttons/Button/Button';
 import { Typography } from 'antd';
 import styled from 'styled-components';
 
@@ -25,4 +26,8 @@ export const SubtitleBalanceText = styled(Typography.Text)`
   font-weight: ${(props) => props.theme.commonFontWeight.regular};
 
   color: ${(props) => props.theme.colors.text.nftLight};
+`;
+
+export const TopUpButton = styled(Button)`
+  ${(props) => props.type === 'ghost' && `color: ${props.theme.colors.text.secondary}`};
 `;
