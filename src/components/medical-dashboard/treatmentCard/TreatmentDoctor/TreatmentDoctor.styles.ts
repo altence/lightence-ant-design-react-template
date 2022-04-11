a9e4a782ad6daa5f744c164d42f2a92719d03b10
leineveber <egor.leineveber@altence.com>
2022-04-12 00:39:19 +0300

    refactor(treatment-card): remove primary color from doctor card

--- src/components/medical-dashboard/treatmentCard/TreatmentDoctor/TreatmentDoctor.styles.ts
+++ src/components/medical-dashboard/treatmentCard/TreatmentDoctor/TreatmentDoctor.styles.ts
@@ -3,7 +3,6 @@ import { Card } from '../../../common/Card/Card';
 import { Col } from 'antd';
 
 export const DoctorName = styled.span`
-  color: ${(props) => props.theme.colors.main.primary};
   font-size: 1.25rem;
   font-weight: bold;
   margin-top: 0.5rem;
@@ -27,11 +26,11 @@ export const LabelCol = styled(Col)`
   opacity: 0.5;
 `;
 
-export const ValueCol = styled(Col)`
-  color: ${(props) => props.theme.colors.main.primary};
-`;
+export const ValueCol = styled(Col)``;
 
 export const Tel = styled.a`
+  color: ${(props) => props.theme.colors.text.main};
+
   &:hover {
     text-decoration: underline;
   }
