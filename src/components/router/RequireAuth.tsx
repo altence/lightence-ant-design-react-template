1d751e787132a2edba0aedbc73a01e5d84df9950
Dmitry Finogeev <dmitry.finogeev@altence.com>
2022-08-08 19:39:28 +0300

    fix: update types

--- src/components/router/RequireAuth.tsx
+++ src/components/router/RequireAuth.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAppSelector } from '@app/hooks/reduxHooks';
+import { WithChildrenProps } from '@app/types/generalTypes';
 
-const RequireAuth: React.FC = ({ children }) => {
+const RequireAuth: React.FC<WithChildrenProps> = ({ children }) => {
   const token = useAppSelector((state) => state.auth.token);
 
   return token ? <>{children}</> : <Navigate to="/auth/login" replace />;
