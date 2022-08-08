f3d88896f920b9cbc767bf49f56e00ebfc24ebd8
Dmitry Finogeev <dmitry.finogeev@altence.com>
2022-08-08 19:39:28 +0300

    fix: update types

--- src/components/common/PageTitle/PageTitle.tsx
+++ src/components/common/PageTitle/PageTitle.tsx
@@ -1,7 +1,8 @@
+import { WithChildrenProps } from '@app/types/generalTypes';
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-export const PageTitle: React.FC = ({ children }) => {
+export const PageTitle: React.FC<WithChildrenProps> = ({ children }) => {
   return (
     <Helmet>
       <title>{children} | Lightence Admin</title>
