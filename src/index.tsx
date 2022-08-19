0564ce0717313e1297193c108d0119ce13983487
Dmitry Finogeev <dmitry.finogeev@altence.com>
2022-08-19 17:13:11 +0300

    fix(react): roll back to 17 version of React

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './i18n';
@@ -6,18 +5,20 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import 'config/config';
 import { Provider } from 'react-redux';
 import { store } from '@app/store/store';
+import ReactDOM from 'react-dom';
+import React from 'react';
 
 interface EventTarget {
   state?: 'activated';
 }
 
-const container = document.getElementById('root') as HTMLElement;
-const root = createRoot(container);
-
-root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 serviceWorkerRegistration.register({
