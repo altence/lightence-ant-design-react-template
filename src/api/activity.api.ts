99c91c485031b951b164a8643aa876fc7663fae7
Alexander Rudnik <alexanderrud0603@gmail.com>
2022-08-25 17:15:18 +0300

    fix(med news): reading assets from cloudflare

--- src/api/activity.api.ts
+++ src/api/activity.api.ts
@@ -25,28 +25,28 @@ export const getUserActivities = (): Promise<UserActivity[]> => {
     setTimeout(() => {
       res([
         {
-          image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1645568175/unsplash_t1PQ4fYJu7M_ueosw4.png',
+          image: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_t1PQ4fYJu7M_ueosw4.webp',
           title: 'Cubic#1',
           status: 'sold',
           date: Date.now() - 1000 * 60 * 60 * 24 * 5,
           usd_value: 240,
         },
         {
-          image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1645568179/unsplash_1rBg5YSi00c_ctycjc.png',
+          image: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_1rBg5YSi00c_ctycjc.webp',
           title: 'Ancient Nature',
           status: 'added',
           date: Date.now() - 1000 * 60 * 60 * 24 * 22,
           usd_value: 1360,
         },
         {
-          image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1645568182/unsplash_t55GeRpETn0_s8myd3.png',
+          image: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_t55GeRpETn0_s8myd3.webp',
           title: 'Art of Mind',
           status: 'booked',
           date: Date.now() - 1000 * 60 * 60 * 24 * 156,
           usd_value: 1895,
         },
         {
-          image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1645568184/unsplash_geJHvrH-CgA_n6mmkv.png',
+          image: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_geJHvrH-CgA_n6mmkv.webp',
           title: 'CryptoBox',
           status: 'sold',
           date: Date.now() - 1000 * 60 * 60 * 24 * 31,
@@ -62,28 +62,28 @@ export const getActivities = (): Promise<Activity[]> => {
     setTimeout(() => {
       res([
         {
-          image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1645813445/unsplash_d2w-_1LJioQ_urzhuj.png',
+          image: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_d2w-_1LJioQ_urzhuj.webp',
           title: 'Yellow Light',
           status: 'sold',
           date: Date.now() - 1000 * 60 * 24,
           owner: '@chingu98',
         },
         {
-          image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1645813449/unsplash_1rBg5YSi00c_1_mpz3a7.png',
+          image: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_1rBg5YSi00c_1_mpz3a7.webp',
           title: 'Cult of Nature',
           status: 'added',
           date: Date.now() - 1000 * 60 * 60 * 2,
           owner: '@azukaru1X',
         },
         {
-          image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1645813452/unsplash_GfQEdpIkkuw_vid9mb.png',
+          image: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_GfQEdpIkkuw_vid9mb.webp',
           title: 'Match the Eyes',
           status: 'booked',
           date: Date.now() - 1000 * 60 * 60 * 22,
           owner: '@samsam',
         },
         {
-          image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1645813455/unsplash_3MAmj1ZKSZA_rfbw6u.png',
+          image: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_3MAmj1ZKSZA_rfbw6u.webp',
           title: 'Plan A & CUSTOM X3',
           status: 'sold',
           date: Date.now() - 1000 * 60 * 60 * 8,
@@ -101,40 +101,41 @@ export const getTrendingActivities = (): Promise<TrendingActivity[]> => {
         {
           title: 'TownYTraveler',
           owner: '@akura',
-          image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646228769/unsplash_yhIsPgLfVNU_1_hdauhp.png',
-          avatar: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646228774/unsplash_tmRuRPBiPcA_dlpsh0.png',
+          image: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_yhIsPgLfVNU_1_hdauhp.webp',
+          avatar: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_tmRuRPBiPcA_dlpsh0.webp',
           usd_value: 1045,
         },
         {
           title: 'TownYTraveler',
           owner: '@akura',
-          image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646228777/unsplash_eHUMDkv4q1w_xchurr.png',
-          avatar: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646228780/unsplash_Tgq8oggf0EY_mwyjub.png',
+          image: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_eHUMDkv4q1w_xchurr.webp',
+          avatar: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_Tgq8oggf0EY_mwyjub.webp',
           usd_value: 1045,
         },
         {
           title: 'TownYTraveler',
           owner: '@akura',
-          image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646228783/unsplash_6JQn1G0lMgY_zqqd7q.png',
-          avatar: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646228786/unsplash_nR-rzu8--5M_qwhnht.png',
-          usd_value: 1045,
-        },
-        {
-          title: 'TownYTraveler',
-          owner: '@akura',
-          image:
-            'https://res.cloudinary.com/lapkinthegod/image/upload/v1647286450/milad-fakurian-bMSA5-tLFao-unsplash_js8utz.jpg',
-          avatar:
-            'https://res.cloudinary.com/lapkinthegod/image/upload/v1647286511/salvatore-andrea-santacroce-wGICoyAhEs4-unsplash_dfo8do.jpg',
+          image: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_6JQn1G0lMgY_zqqd7q.webp',
+          avatar: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/unsplash_nR-rzu8--5M_qwhnht.webp',
           usd_value: 1045,
         },
         {
           title: 'TownYTraveler',
           owner: '@akura',
           image:
-            'https://res.cloudinary.com/lapkinthegod/image/upload/v1647286453/javier-miranda-xB2XP29gn10-unsplash_klwx4d.jpg',
+            process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/milad-fakurian-bMSA5-tLFao-unsplash_js8utz.webp',
           avatar:
-            'https://res.cloudinary.com/lapkinthegod/image/upload/v1647286517/simon-lee-hbFKxsIqclc-unsplash_vcv07z.jpg',
+            process.env.REACT_APP_ASSETS_BUCKET +
+            '/lightence-activity/salvatore-andrea-santacroce-wGICoyAhEs4-unsplash_dfo8do.webp',
+          usd_value: 1045,
+        },
+        {
+          title: 'TownYTraveler',
+          owner: '@akura',
+          image:
+            process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/javier-miranda-xB2XP29gn10-unsplash_klwx4d.webp',
+          avatar:
+            process.env.REACT_APP_ASSETS_BUCKET + '/lightence-activity/simon-lee-hbFKxsIqclc-unsplash_vcv07z.webp',
           usd_value: 1045,
         },
       ]);
