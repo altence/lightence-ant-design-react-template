147d9730c1fab1a24c06b988e8ff73d75e96402f
Alexander Rudnik <alexanderrud0603@gmail.com>
2022-08-25 15:44:26 +0300

    fix(trending creators): reading assets from cloudflare

--- src/api/trendingCreators.ts
+++ src/api/trendingCreators.ts
@@ -14,139 +14,136 @@ export const getTrendingCreators = (): Promise<TrendingCreator[]> => {
     setTimeout(() => {
       res([
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646857835/javier-miranda-fUOf_2t8vzQ-unsplash_wgqfif.jpg',
+          url:
+            process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/javier-miranda-fUOf_2t8vzQ-unsplash_wgqfif.webp',
           header: {
             heading: 'Florence Perez',
             subheading: 'Gold and Silver',
-            profileImage:
-              'https://res.cloudinary.com/lapkinthegod/image/upload/v1646831474/unsplash_3TLl_97HNJo_rxyqpa.png',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/unsplash_3TLl_97HNJo_rxyqpa.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646857835/mo-a9-rD_z-Je4-unsplash_c4rmi3.jpg',
+          url: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/mo-a9-rD_z-Je4-unsplash_c4rmi3.webp',
           header: {
             heading: 'Richard Morales',
             subheading: 'Leporalebru',
-            profileImage:
-              'https://res.cloudinary.com/lapkinthegod/image/upload/v1646831474/unsplash_27QcqVqgVg4_dlbok9.png',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/unsplash_27QcqVqgVg4_dlbok9.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/video/upload/v1646926409/pexels-artem-podrez-7049260_1_zzntn7.mp4',
+          url: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/pexels-artem-podrez-7049260_1_zzntn7.mp4',
           type: 'video',
           header: {
             heading: 'May Gauthier',
             subheading: 'Pigeon',
-            profileImage: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1628436647/Rectangle_463_4_gep4lc.jpg',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/Rectangle_463_4_gep4lc.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646926630/roma-kaiuk-u1vda3HaAJE-unsplash_qkeobi.jpg',
+          url: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/roma-kaiuk-u1vda3HaAJE-unsplash_qkeobi.webp',
           header: {
             heading: 'Christianna Buehner',
             subheading: 'Metro',
-            profileImage: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1628436647/Rectangle_463_1_d1omr2.jpg',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/Rectangle_463_1_d1omr2.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/video/upload/v1647286752/pexels-rostislav-uzunov-5680034_1_oun2f5.mp4',
+          url: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/pexels-rostislav-uzunov-5680034_1_oun2f5.mp4',
           type: 'video',
           header: {
             heading: 'Charles Deluvio',
             subheading: 'Render',
-            profileImage: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1628436646/Rectangle_463_3_gl0gcr.jpg',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/Rectangle_463_3_gl0gcr.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646857835/icarius-G8CByeV0_Bo-unsplash_dxxvhh.jpg',
+          url: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/icarius-G8CByeV0_Bo-unsplash_dxxvhh.webp',
           header: {
             heading: 'John Bostick',
             subheading: 'Icarius',
-            profileImage:
-              'https://res.cloudinary.com/lapkinthegod/image/upload/v1646831474/unsplash_4_QFycgpC4c_gjlyfr.png',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/unsplash_4_QFycgpC4c_gjlyfr.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646857836/cash-macanaya-4OLy06seaH4-unsplash_rvjwzp.jpg',
+          url:
+            process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/cash-macanaya-4OLy06seaH4-unsplash_rvjwzp.webp',
           header: {
             heading: 'Jamie Quinones',
             subheading: 'Space Shark',
-            profileImage:
-              'https://res.cloudinary.com/lapkinthegod/image/upload/v1646831474/unsplash_GsGs_FvQyac_d2eupa.png',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/unsplash_GsGs_FvQyac_d2eupa.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646857837/shubham-dhage-WzKi0E3ZFRc-unsplash_q0vi53.jpg',
+          url:
+            process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/shubham-dhage-WzKi0E3ZFRc-unsplash_q0vi53.webp',
           header: {
             heading: 'Richard Sapienza',
             subheading: 'Shubham Dhage',
-            profileImage:
-              'https://res.cloudinary.com/lapkinthegod/image/upload/v1646831474/unsplash_7rriIaBH6JY_wbcv28.png',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/unsplash_7rriIaBH6JY_wbcv28.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646857837/milad-fakurian-czTJj9LWcwI-unsplash_glvqxh.jpg',
+          url:
+            process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/milad-fakurian-czTJj9LWcwI-unsplash_glvqxh.webp',
           header: {
             heading: 'Steven Negrete',
             subheading: 'Milad Fakurian',
-            profileImage:
-              'https://res.cloudinary.com/lapkinthegod/image/upload/v1646831474/unsplash_bodgc6H44FA_jkbga8.png',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/unsplash_bodgc6H44FA_jkbga8.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646857837/mo-NhYj5-Ib7ik-unsplash_njim5c.jpg',
+          url: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/mo-NhYj5-Ib7ik-unsplash_njim5c.webp',
           header: {
             heading: 'Arnold Rader',
             subheading: 'Gratuita Sobre',
-            profileImage:
-              'https://res.cloudinary.com/lapkinthegod/image/upload/v1646831475/unsplash_ym--mSBZ0ro_msnfti.png',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/unsplash_ym--mSBZ0ro_msnfti.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646857838/daniel-olah-idednB9qCrA-unsplash_g9hihn.jpg',
+          url: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/daniel-olah-idednB9qCrA-unsplash_g9hihn.webp',
           header: {
             heading: 'Zelma Strange',
             subheading: 'Tube with Spirals',
-            profileImage:
-              'https://res.cloudinary.com/lapkinthegod/image/upload/v1646831475/unsplash_joINR2EKnvY_jxmpwr.png',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/unsplash_joINR2EKnvY_jxmpwr.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646857939/pan-yunbo-EgL0EtzL0Wc-unsplash_yajkmd.jpg',
+          url: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/pan-yunbo-EgL0EtzL0Wc-unsplash_yajkmd.webp',
           header: {
             heading: 'Helga Blank',
             subheading: 'Matterport',
-            profileImage:
-              'https://res.cloudinary.com/lapkinthegod/image/upload/v1646831475/unsplash_HD8KlyWRYYM_tbjm2d.png',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/unsplash_HD8KlyWRYYM_tbjm2d.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1647285408/milad-fakurian-wNsHBf_bTBo-unsplash_j5lzpg.jpg',
+          url:
+            process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/milad-fakurian-wNsHBf_bTBo-unsplash_j5lzpg.webp',
           header: {
             heading: 'Rihanna Ritter',
             subheading: 'Lorem Ipsum',
-            profileImage: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1628436647/Rectangle_463_2_hgqjsp.jpg',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/Rectangle_463_2_hgqjsp.webp',
           },
           viewed: false,
         },
         {
-          url: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1647347377/maryna-yazbeck-rnwDiYrHtM0-unsplash_rn3fhj.jpg',
+          url:
+            process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/maryna-yazbeck-rnwDiYrHtM0-unsplash_rn3fhj.webp',
           header: {
             heading: 'Joanna Hammer',
             subheading: 'Silver',
-            profileImage: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1644234637/Ellipse_8_y0rdfd.png',
+            profileImage: process.env.REACT_APP_ASSETS_BUCKET + '/lightence-trending/Ellipse_8_y0rdfd.webp',
           },
           viewed: false,
         },
