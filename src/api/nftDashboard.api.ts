ae1cbb79fcba55c74f94989dba3d3270b52ae253
Alexander Rudnik <alexanderrud0603@gmail.com>
2022-08-25 16:03:42 +0300

    fix(recently added NFTs): reading assets from cloudflare

--- src/api/nftDashboard.api.ts
+++ src/api/nftDashboard.api.ts
@@ -1,7 +1,3 @@
-import birthOfUniverse from '@app/assets/images/nft/nft/borth-of-universe.png';
-import starAway from '@app/assets/images/nft/nft/star-away.png';
-import nightDeprivation from '@app/assets/images/nft/nft/night-deprivation.png';
-
 export interface NftItem {
   image: string;
   title: string;
@@ -14,35 +10,35 @@ export const getRecentlyAddedNfts = (): Promise<NftItem[]> => {
   return new Promise((res) => {
     res([
       {
-        image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646942362/ezgif.com-gif-maker_1_na4ogi.gif',
+        image: process.env.REACT_APP_ASSETS_BUCKET + '/recently-added/ezgif.com-gif-maker_1_na4ogi.gif',
         title: 'XCopy Humano',
         author: 'neithanwolf',
         currentBid: 3521,
         currentBidCrypto: 1.63,
       },
       {
-        image: birthOfUniverse,
+        image: process.env.REACT_APP_ASSETS_BUCKET + '/recently-added/borth-of-universe.webp',
         title: 'Borth of Universe',
         author: 'jakeparker',
         currentBid: 3150,
         currentBidCrypto: 1.02,
       },
       {
-        image: starAway,
+        image: process.env.REACT_APP_ASSETS_BUCKET + '/recently-added/star-away.webp',
         title: 'Star Away',
         author: 'nick_johnson',
         currentBid: 7210,
         currentBidCrypto: 2.08,
       },
       {
-        image: nightDeprivation,
+        image: process.env.REACT_APP_ASSETS_BUCKET + '/recently-added/night-deprivation.webp',
         title: 'Night Deprivation',
         author: 'mikeAdamson',
         currentBid: 740,
         currentBidCrypto: 1.56,
       },
       {
-        image: 'https://res.cloudinary.com/lapkinthegod/image/upload/v1646942358/ezgif.com-gif-maker_t2caul.gif',
+        image: process.env.REACT_APP_ASSETS_BUCKET + '/recently-added/ezgif.com-gif-maker_t2caul.gif',
         title: 'Spiral Hole',
         author: 'jo_wo_le',
         currentBid: 311,
