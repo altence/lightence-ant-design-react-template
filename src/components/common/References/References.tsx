03a57930af16238afe27aec76bda95bc9ba687ea
leineveber <egor.leineveber@altence.com>
2022-11-30 14:54:52 +0300

    fix(references): fix wrong prop type

--- src/components/common/References/References.tsx
+++ src/components/common/References/References.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import * as S from './References.styles';
 import { FacebookOutlined, GithubOutlined, LinkedinOutlined, TwitterOutlined } from '@ant-design/icons';
 
-const GithubIcon = S.withStyles(GithubOutlined);
-const TwitterIcon = S.withStyles(TwitterOutlined);
-const FacebookIcon = S.withStyles(FacebookOutlined);
-const LinkedinIcon = S.withStyles(LinkedinOutlined);
-
 export const References: React.FC = () => {
   return (
     <S.ReferencesWrapper>
@@ -22,16 +17,16 @@ export const References: React.FC = () => {
       </S.Text>
       <S.Icons>
         <a href="https://github.com/altence/lightence-admin" target="_blank" rel="noreferrer">
-          <GithubIcon />
+          <GithubOutlined />
         </a>
         <a href="https://twitter.com/altence_team" target="_blank" rel="noreferrer">
-          <TwitterIcon />
+          <TwitterOutlined />
         </a>
         <a href="https://www.facebook.com/groups/altence" target="_blank" rel="noreferrer">
-          <FacebookIcon />
+          <FacebookOutlined />
         </a>
         <a href="https://linkedin.com/company/altence" target="_blank" rel="noreferrer">
-          <LinkedinIcon />
+          <LinkedinOutlined />
         </a>
       </S.Icons>
     </S.ReferencesWrapper>
