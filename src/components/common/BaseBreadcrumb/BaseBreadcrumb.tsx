8b2fc7e397b4c407a9a3376ba20e8eefb81ae711
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-06-20 17:06:03 +0300

    refactor(Breadcrumb): preserve icon size

--- src/components/common/BaseBreadcrumb/BaseBreadcrumb.tsx
+++ src/components/common/BaseBreadcrumb/BaseBreadcrumb.tsx
@@ -1,3 +1,12 @@
-export { BaseBreadcrumb } from './BaseBreadcrumb.styles';
-export type { BreadcrumbProps as BaseBreadcrumbProps } from 'antd';
-export type { BreadcrumbItemType as BaseBreadcrumbItemType } from 'antd/es/breadcrumb/Breadcrumb';
+import { FC } from 'react';
+import type { BreadcrumbProps } from 'antd';
+import type { BreadcrumbItemType } from 'antd/es/breadcrumb/Breadcrumb';
+import * as S from './BaseBreadcrumb.styles';
+
+export type BaseBreadcrumbItemType = BreadcrumbItemType;
+
+export type BaseBreadcrumbProps = BreadcrumbProps;
+
+export const BaseBreadcrumb: FC<BaseBreadcrumbProps> = (props) => {
+  return <S.Breadcrumb {...props} />;
+};
