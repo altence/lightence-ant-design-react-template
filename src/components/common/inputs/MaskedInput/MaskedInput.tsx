83e0686c26cd1742c17b1644949f9a238799a2dd
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-06-26 20:42:48 +0300

    refactor(MaskedInput): preserve styles

---
+++ src/components/common/inputs/MaskedInput/MaskedInput.tsx
@@ -0,0 +1,10 @@
+import { forwardRef, ComponentPropsWithRef } from 'react';
+import type { InputRef } from 'antd';
+import type { MaskedInput as AntdMaskedInput } from 'antd-mask-input';
+import * as S from './MaskedInput.styles';
+
+export type MaskedInputProps = ComponentPropsWithRef<typeof AntdMaskedInput>;
+
+export const MaskedInput = forwardRef<InputRef, MaskedInputProps>((props) => {
+  return <S.MaskedInput {...props} />;
+});
