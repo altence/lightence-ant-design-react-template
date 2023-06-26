03861656ef37f844f2a21bdaca9b7f5abf572cca
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-06-26 20:44:08 +0300

    refactor(BaseSlider): preserve styles

--- src/components/common/BaseSlider/BaseSlider.styles.ts
+++ src/components/common/BaseSlider/BaseSlider.styles.ts
@@ -6,6 +6,10 @@ export const Slider = styled(AntdSlider)`
     background-color: var(--background-base-color) !important;
   }
 
+  &.ant-slider .ant-slider-handle:focus::after {
+    outline: 6px solid var(--ant-primary-color-outline);
+  }
+
   & .ant-slider-mark-text:not(.ant-slider-mark-text-active) {
     color: var(--subtext-color);
   }
