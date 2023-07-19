67681604b72d6d5c55baf606fd217f324d465dcb
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-07-19 14:03:52 +0300

    refactor(MaskedInput): preserve styles

--- src/components/common/inputs/MaskedInput/MaskedInput.styles.ts
+++ src/components/common/inputs/MaskedInput/MaskedInput.styles.ts
@@ -1,17 +1,17 @@
 import styled from 'styled-components';
 import AntdMaskedInput from 'antd-mask-input';
+import { BORDER_RADIUS } from '@app/styles/themes/constants';
 
+// https://github.com/antoniopresto/antd-mask-input/issues/73
 export const MaskedInput = styled(AntdMaskedInput)`
-  &.ant-input {
-    font-family: inherit;
-    color: var(--text-main-color);
-    background-color: var(--background-color);
-    padding: 11.4px 11px;
-    border: 1px solid var(--border-base-color);
-    border-radius: 7px;
+  font-family: inherit;
+  color: var(--text-main-color);
+  background-color: var(--background-color);
+  padding: 11.4px 11px;
+  border: 1px solid var(--border-base-color);
+  border-radius: ${BORDER_RADIUS};
 
-    &::placeholder {
-      color: var(--input-placeholder-color);
-    }
+  &::placeholder {
+    color: var(--input-placeholder-color);
   }
 `;
