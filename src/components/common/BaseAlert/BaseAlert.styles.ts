b3d8fa86e3b48a1efdbd5de1d7619a28c1f8f4ba
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-07-19 23:24:00 +0300

    refactor(BaseAlert): move styles to themeConfig

--- src/components/common/BaseAlert/BaseAlert.styles.ts
+++ src/components/common/BaseAlert/BaseAlert.styles.ts
@@ -2,14 +2,8 @@ import styled from 'styled-components';
 import { Alert as AntAlert } from 'antd';
 
 export const Alert = styled(AntAlert)`
-  color: var(--black);
-
   &.ant-alert {
     padding-inline: 15px;
-  }
-
-  .ant-alert-message {
-    color: var(--black);
   }
 
   .ant-alert-action {
