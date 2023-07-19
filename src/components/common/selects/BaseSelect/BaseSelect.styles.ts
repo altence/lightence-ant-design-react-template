5315e0eeb374591ad04192929d73e86c33bfe504
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-07-19 22:26:18 +0300

    refactor(BaseSelect): remove duplicate styles

--- src/components/common/selects/BaseSelect/BaseSelect.styles.ts
+++ src/components/common/selects/BaseSelect/BaseSelect.styles.ts
@@ -12,57 +12,37 @@ export interface InternalSelectProps {
 export const Select = styled(AntSelect)<InternalSelectProps>`
   width: ${(props) => props.$width && normalizeProp(props.$width)};
 
-  font-weight: ${FONT_WEIGHT.medium};
+  .ant-select-item-option-selected:not(.ant-select-item-option-disabled) {
+    font-weight: ${FONT_WEIGHT.medium};
+  }
 
   box-shadow: ${(props) => props.$shadow && 'var(--box-shadow)'};
 
   &.ant-select-borderless {
-    background: var(--secondary-background-color);
-
+    background: var(--secondary-background-color) !important;
     border-radius: ${BORDER_RADIUS};
   }
 
   .ant-select-selection-placeholder {
     font-size: ${FONT_SIZE.xs};
-
-    color: var(--text-main-color);
   }
 
-  .ant-select-arrow {
-    color: var(--text-main-color);
-  }
-
-  &.ant-select-disabled.ant-select:not(.ant-select-customize-input) .ant-select-selector {
-    color: var(--disabled-color);
-  }
-
-  .ant-select-clear {
-    color: var(--disabled-color);
-  }
-
-  .ant-select-selection-item-remove {
-    color: var(--icon-color);
-    &:hover {
-      color: var(--icon-hover-color);
-    }
-  }
-
-  .ant-select-item-option-disabled {
-    color: var(--disabled-color);
-  }
-
-  &.ant-select-disabled.ant-select-multiple .ant-select-selection-item {
-    color: #bfbfbf;
-    background: var(--background-base-color);
-    border: 1px solid var(--border-base-color);
-    cursor: not-allowed;
+  .ant-select-item {
+    min-height: unset;
+    padding-block: 5px;
   }
 
   &.ant-select-multiple {
+    &.ant-select-disabled .ant-select-selection-item {
+      color: #bfbfbf;
+      border: 1px solid var(--border-base-color);
+    }
+
     .ant-select-selection-item {
       border: 1px solid #f0f0f0;
       margin-inline-end: 11px;
     }
+
     .ant-select-selector {
       padding-inline-start: 11px;
     }
