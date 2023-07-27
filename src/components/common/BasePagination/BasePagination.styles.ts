57f8a85f30ff738a8c66d6a75a751f6c9e25255a
Alexandr Buhlak <48954743+saha80@users.noreply.github.com>
2023-07-27 10:39:09 +0300

    refactor(BasePagination): remove duplicate styles

--- src/components/common/BasePagination/BasePagination.styles.ts
+++ src/components/common/BasePagination/BasePagination.styles.ts
@@ -1,64 +1,62 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Pagination as AntdPagination } from 'antd';
 
 export const Pagination = styled(AntdPagination)`
-  .ant-pagination-item-container .ant-pagination-item-ellipsis {
-    color: var(--disabled-color);
-  }
-
-  :not(.ant-pagination-disabled) {
-    .ant-pagination-item:focus-visible {
-      a {
-        color: var(--primary-color);
+  ${(props) =>
+    !props.disabled &&
+    css`
+      .ant-pagination-item:not(.ant-pagination-item-active) {
+        &:hover,
+        &:focus-visible {
+          a {
+            color: var(--primary-color);
+          }
+        }
       }
-      border-color: var(--primary-color);
-    }
 
-    .ant-pagination-prev,
-    .ant-pagination-next {
-      &:focus-visible {
-        .ant-pagination-item-link {
-          color: var(--primary-color);
+      .ant-pagination-item-active:focus-visible {
+        a {
+          color: var(--ant-primary-5);
+        }
+
+        border-color: var(--ant-primary-5);
+      }
+
+      &.ant-pagination-mini {
+        .ant-pagination-prev,
+        .ant-pagination-next {
+          &:not(.ant-pagination-disabled) {
+            &:hover,
+            &:focus-visible {
+              .ant-pagination-item-link {
+                color: var(--primary-color);
+                background-color: transparent;
+              }
+            }
+          }
+        }
+
+        .ant-pagination-item:not(.ant-pagination-item-active):hover {
+          background-color: transparent;
+        }
+      }
+
+      &:not(.ant-pagination-mini) {
+        .ant-pagination-prev,
+        .ant-pagination-next {
+          &:focus-visible .ant-pagination-item-link {
+            color: var(--primary-color);
+            border-color: var(--primary-color);
+          }
+        }
+
+        .ant-pagination-item:not(.ant-pagination-item-active):focus-visible {
           border-color: var(--primary-color);
         }
       }
-    }
-  }
+    `}
 
-  &.ant-pagination-disabled {
-    .ant-pagination-jump-prev,
-    .ant-pagination-jump-next {
-      &:focus-visible,
-      &:hover {
-        .ant-pagination-item-link-icon {
-          opacity: 0;
-        }
-        .ant-pagination-item-ellipsis {
-          opacity: 1;
-        }
-      }
-    }
-  }
-
-  .ant-pagination-disabled {
-    .ant-pagination-item-link,
-    .ant-pagination-item a {
-      color: var(--disabled-color);
-    }
-  }
-
-  &.ant-pagination.ant-pagination-disabled {
-    .ant-pagination-item-link,
-    .ant-pagination-item a {
-      color: var(--disabled-color);
-    }
-  }
-
-  & .ant-select-arrow {
-    color: var(--disabled-color);
-  }
-
-  .ant-select-disabled.ant-select:not(.ant-select-customize-input) .ant-select-selector {
+  .ant-select-arrow {
     color: var(--disabled-color);
   }
 `;
--- src/components/common/BasePagination/BasePagination.tsx
+++ src/components/common/BasePagination/BasePagination.tsx
@@ -1,8 +1,10 @@
-import { PaginationProps } from 'antd';
+import { PaginationProps, ConfigProvider } from 'antd';
 import * as S from './BasePagination.styles';
 
 export type BasePaginationProps = PaginationProps;
 
-export const BasePagination: React.FC<BasePaginationProps> = (props) => {
-  return <S.Pagination {...props} />;
-};
+export const BasePagination: React.FC<BasePaginationProps> = (props) => (
+  <ConfigProvider theme={{ components: { Pagination: { wireframe: props.size !== 'small' } } }}>
+    <S.Pagination {...props} />
+  </ConfigProvider>
+);
