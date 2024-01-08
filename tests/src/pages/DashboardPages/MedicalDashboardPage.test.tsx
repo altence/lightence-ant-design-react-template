f51bff5aa152a262b5e866364e4ac4594e46759b
DmitryRazhkou <rozhkow.dimas@gmail.com>
2024-01-08 18:13:52 +0400

    fix(tests): add regress tests configuration and two dashboard tests

---
+++ tests/src/pages/DashboardPages/MedicalDashboardPage.test.tsx
@@ -0,0 +1,23 @@
+import '@testing-library/jest-dom';
+import { generateImage } from 'jsdom-screenshot';
+import puppeteer from "puppeteer";
+import fs from 'fs';
+
+describe('MedicalDashboardPage regress test', () => {
+  it('regress test', async () => {
+    const deployUrl = fs.readFileSync("deploy-url.txt", "utf-8");
+    // const deployUrl = 'https://lightence-ant-design-react-template.pages.dev/medical-dashboard'
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto(deployUrl);
+
+    await page.waitForSelector('#news');
+
+    const screenshot = await page.screenshot({ fullPage: true }); 
+
+    expect(screenshot).toMatchImageSnapshot();
+
+    await browser.close();
+  }, 30000);
+});
