1596bc0cc48100aa1b433d531eccccf19448b03f
DmitryRazhkou <rozhkow.dimas@gmail.com>
2024-01-08 19:01:53 +0400

    fix(tests): remove nft dashboard page for test

--- setupTests.ts
+++ setupTests.ts
@@ -3,12 +3,23 @@ import { toMatchImageSnapshot } from 'jest-image-snapshot';
 import matchMediaMock from 'match-media-mock';
 import * as useResponsiveModule from './src/hooks/useResponsive';
 import { WidthCategory } from './src/styles/themes/types';
+import { initReactI18next } from 'react-i18next';
+import i18n from 'i18next';
 
 const matchMedia = matchMediaMock.create();
 
 global.matchMedia = matchMedia;
 
 expect.extend({ toMatchImageSnapshot });
+
+i18n.use(initReactI18next).init({
+  resources: {},
+  lng: 'en',
+  keySeparator: false,
+  interpolation: {
+    escapeValue: false,
+  },
+});
 
 export const setupResponsiveMock = () => {
   const useResponsiveMock = jest.spyOn(useResponsiveModule, 'useResponsive');
