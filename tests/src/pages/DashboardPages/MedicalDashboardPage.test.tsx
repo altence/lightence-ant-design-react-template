09ddf3e26787dff985789115b9a2d0ff5c2143e3
DmitryRazhkou <rozhkow.dimas@gmail.com>
2024-02-12 12:44:23 +0400

    fix(tests): complete nft and medical dashboard tests

--- tests/src/pages/DashboardPages/MedicalDashboardPage.test.tsx
+++ tests/src/pages/DashboardPages/MedicalDashboardPage.test.tsx
@@ -5,19 +5,21 @@ import fs from 'fs';
 
 describe('MedicalDashboardPage regress test', () => {
   it('regress test', async () => {
-    const deployUrl = fs.readFileSync("deploy-url.txt", "utf-8");
-    // const deployUrl = 'https://lightence-ant-design-react-template.pages.dev/medical-dashboard'
+    // const deployUrl = fs.readFileSync("deploy-url.txt", "utf-8");
+    const deployUrl = 'https://lightence-ant-design-react-template.pages.dev/medical-dashboard'
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
     await page.goto(deployUrl);
 
-    // await page.waitForSelector('#news');
-
     const screenshot = await page.screenshot({ fullPage: true }); 
 
-    expect(screenshot).toMatchImageSnapshot();
+    expect(screenshot).toMatchImageSnapshot({
+      comparisonMethod: 'ssim',
+      failureThreshold: 1,
+      failureThresholdType: 'percent'
+    });
 
     await browser.close();
-  });
+  }, 30000);
 });
